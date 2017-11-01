faf7709c2bc7d2bddf10090c605e6af78d7e9ed9
davidpablocohn <david.cohn@gmail.com>
2017-11-01 15:42:01 -0700

    First pass at ComposedWriter. Resolves #2.

---
+++ writers/composed_writer.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python3
+"""Apply zero or more Transforms (in series) to passed records, then
+write them (in parallel threads) using the specified Writers.
+
+Instantiation:
+
+  writer = ComposedWriter(transforms=None, writers, check_format=True)
+
+    transforms     A single Transform, a list of Transforms, or None.
+
+    writers        A single Writer or a list of Writers.
+
+    NOT IMPLEMENTED YET:
+    check_format   If True, attempt to check that Transform/Writer formats
+                   are compatible, and throw a ValueError if they are not.
+                   If check_format is False (the default) the output_format()
+                   of the whole reader will be formats.Unknown.
+Use:
+
+  writer.write(record)
+
+Sample:
+
+  writer = ComposedWriter(transforms=[TimestampTransform(),
+                                      PrefixTransform('gyr1')],
+                          writers=[NetworkWriter(':6221'),
+                                   LogfileWriter('/logs/gyr1')],
+                          check_format=True)
+                          
+NOTE: we make the rash assumption that transforms are thread-safe,
+that is, that no mischief or corrupted internal state will result if
+more than one thread calls a transform at the same time. To be
+thread-safe, a transform must protect any changes to its internal
+state with a non-re-entrant thread lock, as described in the threading
+module. We do *not* make this assumption of our writers, and impose a
+lock to prevent a writer's write() method from being called a second
+time if the first has not yet completed.
+
+"""
+
+import logging
+import sys
+import threading
+
+sys.path.append('.')
+
+from transforms.transform import Transform
+from writers.writer import Writer
+from utils import formats
+
+################################################################################
+class ComposedWriter(Writer):
+  ############################
+  def __init__(self, transforms=[], writers=[], check_format=False):
+
+    # Make transforms a list if it's not. Even if it's only one transform.
+    if not type(transforms) == type([]):
+      self.transforms = [transforms]
+    else:
+      self.transforms = transforms
+
+    # Make writers a list if it's not. Even if it's only one writer.
+    if not type(writers) == type([]):
+      self.writers = [writers]
+    else:
+      self.writers = writers
+
+    # One lock per writer, to prevent us from accidental re-entry if a
+    # new write is requested before the previous one has completed.
+    self.writer_lock = [threading.Lock() for i in range(len(self.writers))]
+    
+    # If they want, check that our writers and transforms have
+    # compatible input/output formats.
+    input_format = formats.Unknown
+    if check_format:
+      raise ValueError('Sorry - ComposedWriter.check_format() not yet '
+                       'implemented!')
+      #output_format = self.check_writer_formats()
+      #if not output_format:
+      #  raise ValueError('ComposedWriter: No common format found '
+      #                   'for passed writers: %s' % [r.output_format()
+      #                                               for r in self.writers])
+    super().__init__(input_format=input_format)
+
+
+  ############################
+  # Grab the appropriate lock and call the appropriate write() method
+  def run_writer(self, index, record):
+    with self.writer_lock[index]:
+      self.writers[index].write(record)
+          
+  ############################
+  # Apply the transforms in series.
+  def apply_transforms(self, record):
+    if record:
+      for t in self.transforms:
+        record = t.transform(record)
+        if not record:
+          break
+    return record
+
+  ############################
+  # Transform the passed record and dispatch it to writers.
+  def write(self, record):
+    # Transforms run in series
+    record = self.apply_transforms(record)
+    if record is None:
+      return
+
+    # No idea why someone would instantiate without writers, but it's
+    # plausible. Try to be accommodating.
+    if not self.writers:
+      return
+    
+    # If we only have one writer, there's no point making things
+    # complicated. Just write and return.
+    if len(self.writers) == 1:
+      self.writers[0].write(record)
+      return
+
+    # Fire record off to write() requests for each writer.
+    for i in range(len(self.writers)):
+      threading.Thread(target=self.run_writer, args=(i, record)).start()
+
+  ############################
+  # Check that Writer outputs are compatible with each other and with
+  # Transform inputs. Throw an exception if not.
+  def check_writer_formats(self):
+    # Find the lowest common format among writers
+    lowest_common = self.writers[0].output_format()
+    for writer in self.writers:
+       lowest_common = writer.output_format().common(lowest_common)
+       if not lowest_common:
+         return None
+  
+    logging.debug('Lowest common format for writers is "%s"', lowest_common)
+    if not self.transforms:
+      return lowest_common
+    
+    # Now check the transforms in series - output of each is input of
+    # next one.
+    for transform in self.transforms:
+      if not transform.input_format().can_accept(lowest_common):
+        logging.error('Transform %s can not accept input format %s',
+                      transform, lowest_common)
+        return None
+      lowest_common = transform.output_format()
+
+    # Our final format is the lowest common format from last transform
+    return lowest_common
---
+++ writers/test_composed_writer.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+import logging
+import sys
+import tempfile
+import threading
+import unittest
+import warnings
+
+sys.path.append('.')
+
+from writers.text_file_writer import TextFileWriter
+from writers.composed_writer import ComposedWriter
+from writers.writer import Writer
+from transforms.prefix_transform import PrefixTransform
+from utils import formats
+
+SAMPLE_DATA = ['f1 line 1',
+               'f1 line 2',
+               'f1 line 3']
+
+################################################################################
+class TestComposedWriter(unittest.TestCase):
+
+  ############################
+  # To suppress resource warnings about unclosed files
+  def setUp(self):
+    warnings.simplefilter("ignore", ResourceWarning)
+
+    self.tmpdir = tempfile.TemporaryDirectory()
+    self.tmpdirname = self.tmpdir.name
+    logging.info('created temporary directory "%s"', self.tmpdirname)
+
+  ############################
+  def test_check_format(self):
+    f1_name = self.tmpdirname + '/f1'
+    f2_name = self.tmpdirname + '/f2'
+
+    # This should fail
+    with self.assertRaises(ValueError):
+      writer = ComposedWriter(transforms=[],
+                              writers=[TextFileWriter(f1_name),
+                                       TextFileWriter(f2_name)],
+                              check_format=True)
+    
+  ############################
+  def test_all_files(self):
+
+    f1_name = self.tmpdirname + '/f1'
+    f2_name = self.tmpdirname + '/f2'
+    writer = ComposedWriter(transforms=[],
+                            writers=[TextFileWriter(f1_name),
+                                     TextFileWriter(f2_name)])
+
+    f1 = open(f1_name, 'r')
+    f2 = open(f2_name, 'r')
+    
+    for line in SAMPLE_DATA:
+      writer.write(line)
+
+      f1_line = f1.readline().rstrip()
+      f2_line = f2.readline().rstrip()
+
+      self.assertEqual(line, f1_line)
+      self.assertEqual(line, f2_line)
+
+  ############################
+  def test_prefix(self):
+
+    prefix_1 = PrefixTransform('p1')
+    prefix_2 = PrefixTransform('p2')
+
+    f1_name = self.tmpdirname + '/f1'
+    f2_name = self.tmpdirname + '/f2'
+    writer = ComposedWriter(transforms=[prefix_1, prefix_2],
+                            writers=[TextFileWriter(f1_name),
+                                     TextFileWriter(f2_name)])
+
+    f1 = open(f1_name, 'r')
+    f2 = open(f2_name, 'r')
+    
+    for line in SAMPLE_DATA:
+      writer.write(line)
+
+      f1_line = f1.readline().rstrip()
+      f2_line = f2.readline().rstrip()
+
+      self.assertEqual('p2 p1 ' + line, f1_line)
+      self.assertEqual('p2 p1 ' + line, f2_line)
+
+    
+if __name__ == '__main__':
+  import argparse
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-v', '--verbosity', dest='verbosity',
+                      default=0, action='count',
+                      help='Increase output verbosity')
+  args = parser.parse_args()
+
+  LOGGING_FORMAT = '%(asctime)-15s %(message)s'
+  logging.basicConfig(format=LOGGING_FORMAT)
+
+  LOG_LEVELS ={0:logging.WARNING, 1:logging.INFO, 2:logging.DEBUG}
+  args.verbosity = min(args.verbosity, max(LOG_LEVELS))
+  logging.getLogger().setLevel(LOG_LEVELS[args.verbosity])
+  
+  unittest.main(warnings='ignore')
+    
