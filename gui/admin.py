5db09ae5a9d51c7fd317c1c0a128dbdbb70edd8f
davidpablocohn <david.cohn@gmail.com>
2018-03-15 21:29:55 -0700

    Add list filters to /admin page to make it easier to view how a specific logger's state or config has changed over time.

--- gui/admin.py
+++ gui/admin.py
@@ -4,6 +4,7 @@ from .models import Logger, LoggerConfig, LoggerConfigState
 from .models import Mode, Cruise, CurrentCruise, CruiseState
 from .models import ServerState, ServerMessage, StatusUpdate
 
+#############################################
 class ModeInline(admin.TabularInline):
   model = Mode
   extra = 0
@@ -27,6 +28,34 @@ class LoggerConfigInline(admin.TabularInline):
                      'fields': ['config_json']})
     ]
   """
+
+#############################################
+class LoggerAdmin(admin.ModelAdmin):
+  list_display = ('name', 'cruise', 'desired_config', 'current_config')
+
+class LoggerConfigAdmin(admin.ModelAdmin):
+  list_display = ('name', 'logger', 'mode', 'enabled', 'config_json')
+  list_filter = ('logger', 'mode', 'enabled')
+
+class LoggerConfigStateAdmin(admin.ModelAdmin):
+  list_display = ('timestamp', 'config', 'running', 'desired',
+                  'process_id', 'errors')
+  list_filter = ('config__logger', 'config', 'running', 'desired')
+  
+#############################################
+class ModeAdmin(admin.ModelAdmin):
+  def configs(self, obj):
+    return Mode.objects.filter(mode=obj)
+
+  list_display = ('name', 'cruise')
+  fieldsets = [
+    (None,    {'fields':['name', 'cruise']}),
+    #('JSON Source', {'classes': ['collapse'],
+    #                 'fields': ['config_json']})
+    ]
+  inlines = [ LoggerConfigInline ]
+
+#############################################
 class CruiseAdmin(admin.ModelAdmin):
   def modes(self, obj):
     return Mode.objects.filter(cruise=obj)
@@ -43,39 +72,39 @@ class CruiseAdmin(admin.ModelAdmin):
     ]
   inlines = [ ModeInline ]
 
-class ModeAdmin(admin.ModelAdmin):
-  def configs(self, obj):
-    return Mode.objects.filter(mode=obj)
-
-  list_display = ('name', 'cruise')
-  fieldsets = [
-    (None,    {'fields':['name', 'cruise']}),
-    #('JSON Source', {'classes': ['collapse'],
-    #                 'fields': ['config_json']})
-    ]
-  inlines = [ LoggerConfigInline ]
-
 class CurrentCruiseAdmin(admin.ModelAdmin):
   list_display = ('cruise', 'as_of')
 
+class CruiseStateAdmin(admin.ModelAdmin):
+  list_display = ('cruise', 'current_mode', 'started')
+  list_filter = ('cruise',)
+
+#############################################
 class ServerStateAdmin(admin.ModelAdmin):
   list_display = ('timestamp', 'server', 'running', 'desired', 'process_id')
+  list_filter = ('server', 'running', 'desired')
 
 class ServerMessageAdmin(admin.ModelAdmin):
   list_display = ('timestamp', 'server', 'message')
+  list_filter = ('server',)
 
 class StatusUpdateAdmin(admin.ModelAdmin):
   list_display = ('timestamp', 'server', 'cruise', 'status')
 
+#############################################
+admin.site.register(Logger, LoggerAdmin)
+admin.site.register(LoggerConfig, LoggerConfigAdmin)
+admin.site.register(LoggerConfigState, LoggerConfigStateAdmin)
+
+admin.site.register(Mode, ModeAdmin)
 
 admin.site.register(Cruise, CruiseAdmin)
-admin.site.register(Mode, ModeAdmin)
 admin.site.register(CurrentCruise, CurrentCruiseAdmin)
+admin.site.register(CruiseState, CruiseStateAdmin)
+
 admin.site.register(ServerState, ServerStateAdmin)
 admin.site.register(ServerMessage, ServerMessageAdmin)
 admin.site.register(StatusUpdate, StatusUpdateAdmin)
 
-admin.site.register(Logger)
-admin.site.register(LoggerConfig)
-admin.site.register(LoggerConfigState)
-admin.site.register(CruiseState)
+
+
