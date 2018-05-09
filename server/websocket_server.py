95d7344fc857ab39219f50f198c65cf6822b9f95
davidpablocohn <david.cohn@gmail.com>
2018-05-08 21:46:55 -0700

    Functioning websocket server, now with instructions in file header.

--- server/websocket_server.py
+++ server/websocket_server.py
@@ -1,5 +1,43 @@
 #!/usr/bin/env python3
+"""A simple websocket server. Producer and consumer routines may be
+passed in that will generate messages to be sent to the websocket
+(producer) and will take messages retrieved from the websocket and
+process them (consumer).
 
+The attached executable script uses default routines that look for
+messages from a producer_queue to send, and add received messages to a
+consumer_queue. When given a command line, the script's behavior is to
+send it out to *all* attached clients. Retrieved messages are queued,
+but nothing is done with them.
+
+To run, try:
+
+    server/websocket_server.py --websocket localhost:8765 -v
+
+In a separate window, start up the simulated serial ports:
+
+    logger/utils/simulate_serial.py --config test/serial_sim.json -v
+
+In a third window, start up a LoggerManager as a websocket client:
+
+    server/logger_manager.py --websocket localhost:8765 -v 
+
+You should then be able to send commands to the LoggerManager from the
+websocket server. Try:
+
+    load_cruise test/configs/sample_cruise.json
+
+    set_mode port
+
+    set_mode off
+
+    quit
+
+(You can verify that the loggers are starting/stopping as desired by firing up a listener in yet *another* window:
+
+    logger/listener/listen.py --network :6224 --write_file -
+)
+"""
 import asyncio
 import json
 import logging
@@ -13,65 +51,64 @@ import websockets
 class WebsocketServer:
 
   ############################
-  def __init__(self, host, port, consumer=None, producer=None):
-    """
-    host, port - host and port to open as websocket
+  def __init__(self, host, port, consumer, producer,
+               on_connect=None, on_disconnect=None):
+    """host, port - host and port to open as websocket
 
-    consumer - async routine that takes a str argument and does something 
-       with it. Strings retrieved from the websocket will be passed to this
-       routine. If omitted, a default consumer will push the string to
-       self.consumer_queue.
+    consumer - async routine that takes a str argument and a
+       client_id and does something with it. Strings retrieved from
+       the websocket will be passed to this routine.
 
-    producer - argument-less async routine that produces the strings we
-       want to send out on the websocket. If omitted, a default consumer 
-       will check self.producer_queue for for commands.
+    producer - async routine that takes a client_id and produces the
+       strings we want to send out to that client on the websocket.
+
+    on_connect - optional routine to be called when a new client connects.
+       Should take two parameters:
+           websocket - the websocket itself, in case someone wants to store it
+           client_id - an integer client id
+
+    on_disconnect - optional routine to be called when a client
+       disconnects. Should take a single integer representing client's
+       unique client_id
+
     """
     self.host = host
     self.port = port
-    self.consumer = consumer or self.queued_consumer
-    self.producer = producer or self.queued_producer
+    self.consumer = consumer
+    self.producer = producer
 
-    self.consumer_queue = queue.Queue()
-    self.producer_queue = queue.Queue()
+    self.on_connect = on_connect
+    self.on_disconnect = on_disconnect
 
-  ############################
-  # Called when we receive a message
-  async def queued_consumer(self, message):
-    logging.debug('Received message for queue: ' + message)
-    self.consumer_queue.put(message)
+    self.client_lock = threading.Lock()
+    self.num_clients = 0
+    self.client_map = {}
 
+    self.quit_requested = False
+    
   ############################
-  # Called when we want something to send via the websocket
-  async def queued_producer(self):
-    while True:
-      try:
-        message = self.producer_queue.get_nowait()
-        if message.strip():
-          logging.info('Sending message from queue: ' + message)
-          return message
-      except queue.Empty:
-        await asyncio.sleep(0.001)
-  
-  ############################
-  async def _consumer_handler(self, websocket):
+  async def _consumer_handler(self, websocket, client_id):
     try:
       async for message in websocket:
         logging.debug('WebsocketServer received message: ' + message)
-        await self.consumer(message)
+        await self.consumer(message, client_id)
+
+        if self.quit_requested:
+          return
     except:
       logging.info('Websocket connection lost')
 
   ############################
-  async def _producer_handler(self, websocket):
+  async def _producer_handler(self, websocket, client_id):
     """Here, we could either await some other producer to give us a command
     or poll."""
     # If we're waiting for an external command producer:
     try:
-      while True:
-        message = await self.producer()
+      while not self.quit_requested:
+        message = await self.producer(client_id)
         if message:
           await websocket.send(message)
-          logging.info('WebsocketServer sent message: ' + message)
+          logging.debug('WebsocketServer sent message: ' + message)
         else:
           await asyncio.sleep(1)
     except websockets.exceptions.ConnectionClosed:
@@ -79,30 +116,149 @@ class WebsocketServer:
 
   ############################
   async def _handler(self, websocket, path):
+    with self.client_lock:
+      client_id = self.num_clients
+      logging.warning('New client #%d attached', client_id)
+      self.client_map[client_id] = websocket
+      self.num_clients += 1
+      if self.on_connect:
+        self.on_connect(websocket, client_id)
+    
     tasks = []
+
+    # Task that receives data from websocket and does something with it
     if self.consumer:
-      tasks.append(asyncio.ensure_future(self._consumer_handler(websocket)))
+      tasks.append(asyncio.ensure_future(self._consumer_handler(websocket,
+                                                                client_id)))
+    # Task that produces data that we're going to send out on websocket
     if self.producer:
-      tasks.append(asyncio.ensure_future(self._producer_handler(websocket)))
+      tasks.append(asyncio.ensure_future(self._producer_handler(websocket,
+                                                                client_id)))
     done, pending = await asyncio.wait(tasks,
                                        return_when=asyncio.FIRST_COMPLETED)
     for task in pending:
       task.cancel()
-    logging.info('WebsocketServer tasks completed')
+
+    # When client disconnects, delete the queues it was using
+    with self.client_lock:
+      logging.info('WebsocketServer client #%d completed', client_id)
+      del self.client_map[client_id]
+      if self.on_disconnect:
+        self.on_disconnect(client_id)
 
   ############################
-  def send(self, message):
-    try:
-      websocket.send(message)
-      logging.info('WebsocketServer sent message: ' + message)
-    except websockets.exceptions.ConnectionClosed:
-      logging.info('Websocket connection lost')
+  def clients(self):
+    """Return a dict mapping client_id->websocket."""
+    return self.client_map
 
   ############################
   def run(self):
     start_server = websockets.serve(self._handler, self.host, self.port)
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
+
+  ############################
+  def quit(self):
+    """NOTE: This doesn't really shut things down because the event loop
+    keeps running."""
+    self.quit_requested = True
+
+################################################################################
+"""Below are tools for a standalone executable that takes messages
+from the command line and sends them to every connected client, and
+receives messages from the client websockets and prints them to the
+console.
+
+To remain non-blocking, it operates via a pair of queues:
+
+  send_queue = {}     # client_id->queue for messages to send to ws
+  receive_queue = {}  # client_id->queue for messages from ws
+
+The websocket server is initialized with a pair of routines that use
+these queues:
+
+  queued_consumer - a consumer that takes a message and "consumes"
+                    them, in this case by pushing them on the
+                    receive_queue for others to process
+
+  queued_producer - a routine that "produces" a message (by pulling
+                    one off the send_queue) for the ws server to send.
+"""
+
+# Queues for our queued consumers/producers
+send_queue = {}     # client_id->queue for messages to send to ws
+receive_queue = {}  # client_id->queue for messages from ws
+
+# A lock to make sure only one thread is messing with the above
+# maps at any given time.
+websocket_map_lock = threading.Lock()
+
+##########################
+# This is a consumer - it takes a message and does something with it
+async def queued_consumer(message, client_id):
+  global receive_queue
+  logging.debug('Received message from client #%d: %s', client_id, message)
+  receive_queue[client_id].put(message)
+
+############################
+# This is a producer - it produces a message (from the queue) to send
+async def queued_producer(client_id):
+  global send_queue
+  while True:
+    try:
+      message = send_queue[client_id].get_nowait()
+      if message.strip():
+        logging.debug('Sending message to client #%d: %s', client_id, message)
+        return message
+    except queue.Empty:
+      await asyncio.sleep(0.1)
+      
+############################
+def register_websocket_client(websocket, client_id):
+  """We've been alerted that a websocket client has connected.
+  Register it properly."""
+  global send_queue, receive_queue, websocket_map_lock
+  with websocket_map_lock:
+    if not client_id in send_queue:
+      send_queue[client_id] = queue.Queue()
+      receive_queue[client_id] = queue.Queue()
+    logging.warning('Websocket client #%d has connected', client_id)
+
+############################
+def unregister_websocket_client(client_id):
+  """We've been alerted that a websocket client has disconnected.
+  Unegister it properly."""
+  global send_queue, receive_queue, websocket_map_lock
+  with websocket_map_lock:
+    if client_id in send_queue: del send_queue[client_id]
+    if client_id in receive_queue: del receive_queue[client_id]
+    logging.warning('Websocket client #%d has disconnected', client_id)
+
+############################
+def read_commands():
+  while not server.quit_requested:
+    command = input('Command? ')
+    for client_id, sender in send_queue.items():
+      logging.warning('Pushing command to client %d: %s', client_id, command)
+      send_queue[client_id].put(command)
+
+    if command == 'quit':
+      logging.warning('Quitting!')
+      server.quit()
+
+############################
+def process_results():
+  SHOW_LEN = 30
+  while not server.quit_requested:
+    with server.client_lock:
+      for client_id, receiver in receive_queue.items():
+        try:
+          message = receive_queue[client_id].get_nowait()
+          logging.info('#%d: %s%s', client_id, message[:SHOW_LEN],
+                       '' if len(message) < SHOW_LEN else '...')
+        except queue.Empty:
+          pass
+      time.sleep(0.1)
 
 ################################################################################
 if __name__ == '__main__':
@@ -133,19 +289,23 @@ if __name__ == '__main__':
     logging.error('--websocket arg "%s" not in host:port format',args.websocket)
     exit(1)
 
-  # If we don't pass in consumer and/or producer, WebsocketServer will
-  # use its queues
-  server = WebsocketServer(host=host, port=port)
+  # Create the websocket server, setting up queued senders/receivers
+  server = WebsocketServer(host=host, port=port,
+                           consumer=queued_consumer, producer=queued_producer,
+                           on_connect=register_websocket_client,
+                           on_disconnect=unregister_websocket_client)
 
-  def read_commands():
-    while True:
-      command = input('Command? ')
-      server.producer_queue.put(command)
-
-  read_command_thread = threading.Thread(target=read_commands,
-                                         name='read_commands_thread')
+  read_command_thread = threading.Thread(target=read_commands)
   read_command_thread.start()
+  process_results_thread = threading.Thread(target=process_results)
+  process_results_thread.start()
 
   # Start websocket server
-  server.run()
+  try:
+    server.run()
+  except KeyboardInterrupt:
+    logging.warning('Got interrupt')
+
+  read_command_thread.join()
+  process_results_thread.join()
   
