009b8f45eb2e1935e99346c369500687c4e5f227
davidpablocohn <david.cohn@gmail.com>
2018-06-25 13:33:19 -0700

    Added assertRecursiveAlmostEqual to cover for numerical differences in computation

--- logger/transforms/test_true_winds_transform.py
+++ logger/transforms/test_true_winds_transform.py
@@ -178,6 +178,39 @@ SANITY_RESULTS = [
 
 class TestTrueWindsTransform(unittest.TestCase):
   ############################
+  def assertRecursiveAlmostEqual(self, val1, val2, max_diff=0.00001):
+    """Assert that two values/dicts/lists/sets are almost equal. That is,
+    that their non-numerical entries are equal, and that their
+    numerical entries are equal to within max_diff. NOTE: does not
+    detect 'almost' equal for sets.
+    """
+    if type(val1) in (int, float) and type(val2) in (int, float):
+      self.assertLess(abs(val1-val2), max_diff)
+      return
+    
+    if type(val1) in (str, bool, type(None)):
+      self.assertEqual(val1, val2)
+      return
+
+    # If here, it should be a list, set or dict
+    self.assertTrue(type(val1) in (set, list, dict))
+    self.assertEqual(type(val1), type(val2))
+    self.assertEqual(len(val1), len(val2))
+    
+    if type(val1) == list:
+      for i in range(len(val1)):
+        self.assertRecursiveAlmostEqual(val1[i], val2[i], max_diff)
+
+    elif type(val1) == set:
+      for v in val1:
+        self.assertTrue(v in val2)
+
+    elif type(val1) == dict:
+      for k in val1:
+        self.assertTrue(k in val2)
+        self.assertRecursiveAlmostEqual(val1[k], val2[k], max_diff)
+
+  ############################
   def test_default(self):
     lines = LINES.copy()
     expected_results = RESULTS.copy()
@@ -213,7 +246,7 @@ class TestTrueWindsTransform(unittest.TestCase):
         self.assertIsNone(result)
         self.assertIsNone(expected)
       else:
-        self.assertDictEqual(result, expected)
+        self.assertRecursiveAlmostEqual(result, expected)
 
   ############################
   def test_no_timestamps(self):
@@ -251,7 +284,7 @@ class TestTrueWindsTransform(unittest.TestCase):
         self.assertIsNone(result)
         self.assertIsNone(expected)
       else:
-        self.assertDictEqual(result, expected)
+        self.assertRecursiveAlmostEqual(result, expected)
 
   ############################
   def test_sanity(self):
@@ -275,7 +308,7 @@ class TestTrueWindsTransform(unittest.TestCase):
       expected = expected_results.pop(0)
       logging.info('sanity result: %s', result)
       logging.info('sanity expected: %s', expected)
-      self.assertDictEqual(result, expected)
+      self.assertRecursiveAlmostEqual(result, expected)
 
     return
   
