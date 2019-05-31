ba1e8a32a91c8adb07c85390aa85086325611370
davidpablocohn <david.cohn@gmail.com>
2019-05-31 16:12:03 -0700

    NetworkReader now accepts optional eol character. If provided, with buffer/split messages to return one record plus eol per read call. Resolves #108. I think.

--- logger/readers/network_reader.py
+++ logger/readers/network_reader.py
@@ -9,7 +9,7 @@ from os.path import dirname, realpath; sys.path.append(dirname(dirname(dirname(r
 from logger.utils.formats import Text
 from logger.readers.reader import Reader
 
-BUFFER_SIZE = 4096
+READ_BUFFER_SIZE = 4096 # max number of characters to read in one call
 
 ################################################################################
 # Read to the specified file. If filename is empty, read to stdout.
@@ -25,20 +25,33 @@ class NetworkReader(Reader):
   aggregate recv()'s and know when it's got an entire record?
   """
   ############################
-  def __init__(self, network, buffer_size=BUFFER_SIZE):
+  def __init__(self, network, eol=None, read_buffer_size=READ_BUFFER_SIZE):
     """
-
     network      Network address to read, in host:port format (e.g.
                  'rvdas:6202'). If host is omitted (e.g. ':6202'),
                  read via UDP on specified port.
+
+    eol          If not specified, assume one record per network packet.  If
+                 specified, buffer network reads until the eol
+                 character has been seen, and return the entire record
+                 at once, retaining everything after the eol for the
+                 start of the subsequent record. If multiple eol characters
+                 are encountered in a packet, split the packet and return
+                 the first of them, buffering the remainder for subsequent
+                 calls.
     """
     super().__init__(output_format=Text)
 
     self.network = network
-    self.buffer_size = buffer_size
+    self.eol = eol
+    self.read_buffer_size = read_buffer_size
+
+    # Where we'll aggregate incomplete records if an eol char is specified
+    self.record_buffer = ''
+
     if network.find(':') == -1:
-      raise ValueError('NetworkReader network argument must be in '
-                       '\'host:port\' or \':port\' format. Found "%s"', network)
+      raise ValueError('NetworkReader network argument must be in \'host:port\''
+                       ' or \':port\' format. Found "%s"' % network)
     (host, port) = network.split(':')
     port = int(port)
 
@@ -71,8 +84,30 @@ class NetworkReader(Reader):
     """
     Read the next network packet.
     """
-    record = self.socket.recv(self.buffer_size)
-    logging.debug('NetworkReader.read() received %d bytes', len(record))
-    if record:
-      record = record.decode('utf-8')
-    return record
+    # If no eol character/string specified, just read a packet and
+    # return it as the next record.
+    if not self.eol:
+      record = self.socket.recv(self.read_buffer_size)
+      logging.debug('NetworkReader.read() received %d bytes', len(record))
+      if record:
+        record = record.decode('utf-8')
+      return record
+
+    # If an eol character/string has been specified, we may have to
+    # loop our reads until we see an eol.
+    while True:
+      eol_pos = self.record_buffer.find(self.eol)
+      if eol_pos > -1:
+        # We have an eol string somewhere in our buffer. Return
+        # everything up to it.
+        record_end = eol_pos + len(self.eol)
+        record = self.record_buffer[0:record_end]
+        logging.debug('NetworkReader found eol; returning record')
+        self.record_buffer = self.record_buffer[record_end:]
+        return record
+
+      # If no eol string, read, append, and try again.
+      record = self.socket.recv(self.read_buffer_size)
+      logging.debug('NetworkReader.read() received %d bytes', len(record))
+      if record:
+        self.record_buffer += record.decode('utf-8')
