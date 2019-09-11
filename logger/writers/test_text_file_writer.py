35c97852247eba27ef2949450328cee2c386f424
davidpablocohn <david.cohn@gmail.com>
2019-09-10 22:18:45 -0400

    Allows TextFileWriter to split text files by date. Resolves #154.

--- logger/writers/test_text_file_writer.py
+++ logger/writers/test_text_file_writer.py
@@ -28,6 +28,22 @@ def create_file(filename, lines, interval=0, pre_sleep_interval=0):
     f.flush()
   f.close()
 
+########################################################
+class TodayTextFileWriter(TextFileWriter):
+  """A hacked version of TextFileWriter where we can tweak what "today" is."""
+  def __init__(self, filename=None, flush=True, truncate=False,
+               split_by_date=False, create_path=True):
+    self.today = (2019, 9, 9)
+    super().__init__(filename=filename, flush=flush, truncate=truncate,
+                     split_by_date=split_by_date, create_path=create_path)
+    
+  ############################
+  # Override _today() to return whatever we set it as
+  def _today(self):
+    """Manual version of _today()."""
+    return self.today
+
+########################################################
 class TestTextFileWriter(unittest.TestCase):
 
   ############################
@@ -58,6 +74,27 @@ class TestTextFileWriter(unittest.TestCase):
 
     with self.assertRaises(TypeError):
       writer.input_format('not a format')
+
+  ############################
+  def test_split(self):
+    """Test the split_by_date parameter, changing the date with each write."""
+    with tempfile.TemporaryDirectory() as tmpdirname:
+      writer = TodayTextFileWriter(tmpdirname + '/g', split_by_date=True)
+
+      writer.today = (2019, 5, 1)
+      writer.write(SAMPLE_DATA[0])
+      with open(tmpdirname + '/g-%04d-%02d-%02d' % writer.today) as f:
+        self.assertEqual(SAMPLE_DATA[0], f.readline().strip())
+
+      writer.today = (2019, 5, 3)
+      writer.write(SAMPLE_DATA[1])
+      with open(tmpdirname + '/g-%04d-%02d-%02d' % writer.today) as f:
+        self.assertEqual(SAMPLE_DATA[1], f.readline().strip())
+
+      writer.today = (2019, 5, 9)
+      writer.write(SAMPLE_DATA[2])
+      with open(tmpdirname + '/g-%04d-%02d-%02d' % writer.today) as f:
+        self.assertEqual(SAMPLE_DATA[2], f.readline().strip())
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
