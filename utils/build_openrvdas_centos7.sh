ce3562640427d08bc85af3da7f67b0efdcaa79d8
davidpablocohn <david.cohn@gmail.com>
2019-11-14 16:10:45 -0800

    Remove pip3 update command.

--- utils/build_openrvdas_centos7.sh
+++ utils/build_openrvdas_centos7.sh
@@ -250,8 +250,7 @@ echo "##########################################################################
 
 echo Installing Django, uWSGI and other Python-dependent packages
 export PATH=/usr/bin:/usr/local/bin:$PATH
-/usr/bin/env pip3 install --upgrade pip
-/usr/local/bin/pip3 install Django==2.2 pyserial uwsgi \
+/usr/bin/pip3 install Django==2.2 pyserial uwsgi \
                    websockets PyYAML parse mysqlclient mysql-connector diskcache
 # uWSGI configuration
 #Following instructions in https://www.tecmint.com/create-new-service-units-in-systemd/
