7d4c4704561bbbd1738a0e9b6430eb637296d1a0
Webb Pinner <webbpinner@gmail.com>
2020-01-17 08:07:26 -0500

    Add ability to truncate log files by hour (#191)
    * Add ability to truncate log files by hour
    
    Some users may want hourly files instead of daily.  This will be true for larger datasets.  These changes allow users to easily declare if they want hourly files.
    
    * Update logfile_writer.py
    
    * added suffix code to add_rollover_interval branch

--- logger/writers/logfile_writer.py
+++ logger/writers/logfile_writer.py
@@ -14,20 +14,28 @@ class LogfileWriter(Writer):
   """Write to the specified file. If filename is empty, write to stdout."""
   def __init__(self, filebase=None, flush=True,
                time_format=timestamp.TIME_FORMAT,
-               date_format=timestamp.DATE_FORMAT):
+               date_format=timestamp.DATE_FORMAT,
+               suffix='',
+               rollover_hourly=False):
     """
     Write timestamped text records to file. Base filename will have
     date appended, in keeping with R2R format recommendations
     (http://www.rvdata.us/operators/directory). When timestamped date on
     records rolls over to next day, create new file with new date suffix.
     ```
-    filebase     Base name of file to write to. Will have record date
-                 appended, e.g.
+    filebase        Base name of file to write to. Will have record date
+                    appended, e.g.
 
-    flush        If True (default), flush after every write() call
+    flush           If True (default), flush after every write() call
 
-    date_fomat   A strftime-compatible string, such as '%Y-%m-%d'; defaults
-                 to whatever's defined in utils.timestamps.DATE_FORMAT.
+    date_fomat      A strftime-compatible string, such as '%Y-%m-%d';
+                    defaults to whatever's defined in
+                    utils.timestamps.DATE_FORMAT.
+
+    suffix          string to apply to the end of the log filename
+
+    rollover_hourly Set files to truncate by hour.  By default files will
+                    truncate by day
     ```
     """
     super().__init__(input_format=Text)
@@ -36,8 +44,11 @@ class LogfileWriter(Writer):
     self.flush = flush
     self.time_format = time_format
     self.date_format = date_format
+    self.suffix = suffix
+    self.rollover_hourly = rollover_hourly
 
     self.current_date = None
+    self.current_hour = None
     self.current_filename = None
     self.writer = None
 
@@ -63,6 +74,7 @@ class LogfileWriter(Writer):
     try:
       time_str = record.split()[0]
       ts = timestamp.timestamp(time_str, time_format=self.time_format)
+      hr_str = self.rollover_hourly and timestamp.date_str(ts, date_format='_%H00') or ""      
       date_str = timestamp.date_str(ts, date_format=self.date_format)
       logging.debug('LogfileWriter date_str: %s', date_str)
     except ValueError:
@@ -70,9 +82,10 @@ class LogfileWriter(Writer):
       return
 
     # Is it time to create a new file to write to?
-    if not self.writer or date_str != self.current_date:
-      self.current_filename = self.filebase + '-' + date_str
+    if not self.writer or date_str != self.current_date or hr_str != self.current_hour:
+      self.current_filename = self.filebase + '-' + date_str + hr_str + self.suffix
       self.current_date = date_str
+      self.current_hour = self.rollover_hourly and hr_str or ""
       logging.info('LogfileWriter opening new file: %s', self.current_filename)
       self.writer = TextFileWriter(self.current_filename, self.flush)
 
