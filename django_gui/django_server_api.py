c20c0f2367b6666e9d71017923a552526a9e3345
davidpablocohn <david.cohn@gmail.com>
2020-02-05 13:49:33 -0800

    Wrap get_loggers and load_configuration in transactions so they can't step on each other. A test at solving #197.

--- django_gui/django_server_api.py
+++ django_gui/django_server_api.py
@@ -22,7 +22,7 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_gui.settings')
 django.setup()
 
-from django.db import connection
+from django.db import connection, transaction
 
 from .models import Logger, LoggerConfig, LoggerConfigState
 from .models import Mode, Cruise
@@ -256,21 +256,26 @@ class DjangoServerAPI(ServerAPI):
     """Get a dict of {logger_id:logger_spec,...} defined for the
     current cruise."""
     with self.config_rlock:
-      while True:
-        try:
-          loggers = Logger.objects.all()
-          if not loggers:
-            raise ValueError('No loggers found in cruise')
-          return {
-            logger.name:{'configs': self.get_logger_config_names(logger.name),
-                         'active': self.get_logger_config_name(logger.name)}
-            for logger in loggers
-          }
-        except django.db.utils.OperationalError as e:
-          logging.warning('_get_loggers() '
-                          'Got DjangoOperationalError. Trying again: %s', e)
-          connection.close()
-          time.sleep(0.1)
+      with transaction.atomic():
+        while True:
+          try:
+            # Note: we're not actually updating, but we want to be
+            # exclusive of the transaction in load_configuration().
+            loggers = Logger.objects.select_for_update().all()
+
+            if not loggers:
+              raise ValueError('No loggers found in cruise')
+            logger_configs = {
+              logger.name:{'configs': self.get_logger_config_names(logger.name),
+                           'active': self.get_logger_config_name(logger.name)}
+              for logger in loggers
+            }
+            return logger_configs
+          except django.db.utils.OperationalError as e:
+            logging.warning('_get_loggers() '
+                            'Got DjangoOperationalError. Trying again: %s', e)
+            connection.close()
+            time.sleep(0.1)
 
   ############################
   def get_logger_config(self, config_name):
@@ -659,108 +664,113 @@ class DjangoServerAPI(ServerAPI):
     """Add a complete cruise configuration (id, modes, configs,
     default) to the data store."""
     with self.config_rlock:
-      cruise_def = configuration.get('cruise', {})
-      loggers = configuration.get('loggers', None)
-      modes = configuration.get('modes', None)
-      default_mode = configuration.get('default_mode', None)
-      configs = configuration.get('configs', None)
+      with transaction.atomic():
+        cruise_def = configuration.get('cruise', {})
+        loggers = configuration.get('loggers', None)
+        modes = configuration.get('modes', None)
+        default_mode = configuration.get('default_mode', None)
+        configs = configuration.get('configs', None)
 
-      if loggers is None:
-        raise ValueError('Cruise definition has no loggers')
-      if modes is None:
-        raise ValueError('Cruise definition has no modes')
-      if configs is None:
-        raise ValueError('Cruise definition has no configs')
+        if loggers is None:
+          raise ValueError('Cruise definition has no loggers')
+        if modes is None:
+          raise ValueError('Cruise definition has no modes')
+        if configs is None:
+          raise ValueError('Cruise definition has no configs')
 
-      # We're going in - don't let anyone touch config until we're done
+        # We're going in - a select_for_update() locks the Logger table
+        # so no one else can mess until we're done with the transaction.
+        loggers_lock = Logger.objects.select_for_update().all()
+
+          ################
+        # Begin by creating the Cruise object. If no cruise name, just
+        # call it 'Cruise'.
+        cruise_id = cruise_def.get('id', None)
+        if cruise_id is None:
+          cruise_id = 'Cruise'
+
+        # Delete old cruise. There should be only one, but...
+        for old_cruise in Cruise.objects.all():
+          logging.info('Deleting old cruise "%s"', old_cruise.id)
+          old_cruise.delete()
+
+        cruise = Cruise(id=cruise_id)
+        start_time = cruise_def.get('start', None)
+        if start_time:
+          cruise.start = datetime_obj(start_time, time_format=DATE_FORMAT)
+        end_time = cruise_def.get('end', None)
+        if end_time:
+          cruise.end = datetime_obj(end_time, time_format=DATE_FORMAT)
+        cruise.save()
 
         ################
-      # Begin by creating the Cruise object. If no cruise name, just
-      # call it 'Cruise'.
-      cruise_id = cruise_def.get('id', None)
-      if cruise_id is None:
-        cruise_id = 'Cruise'
+        # Create the modes
+        for mode_name in modes:
+          logging.info('  Creating mode %s (cruise %s)', mode_name, cruise_id)
+          mode = Mode(name=mode_name, cruise=cruise)
+          mode.save()
 
-      # Delete old cruise. There should be only one, but...
-      for old_cruise in Cruise.objects.all():
-        logging.info('Deleting old cruise "%s"', old_cruise.id)
-        old_cruise.delete()
+          # Is this mode the default mode for the cruise?
+          if mode_name == default_mode:
+            logging.info('    Setting %s as default mode', mode_name)
+            cruise.default_mode = mode
+            cruise.save()
 
-      cruise = Cruise(id=cruise_id)
-      start_time = cruise_def.get('start', None)
-      if start_time:
-        cruise.start = datetime_obj(start_time, time_format=DATE_FORMAT)
-      end_time = cruise_def.get('end', None)
-      if end_time:
-        cruise.end = datetime_obj(end_time, time_format=DATE_FORMAT)
-      cruise.save()
+        ################
+        # Create the loggers
+        for logger_name, logger_spec in loggers.items():
+          logging.info('Creating logger %s (cruise %s)', logger_name, cruise_id)
+          logger = Logger(name=logger_name, cruise=cruise)
+          logger.save()
 
-      ################
-      # Create the modes
-      for mode_name in modes:
-        logging.info('  Creating mode %s (cruise %s)', mode_name, cruise_id)
-        mode = Mode(name=mode_name, cruise=cruise)
-        mode.save()
+          # Create and associate all relevant modes for the logger
+          logger_configs = logger_spec.get('configs', None)
+          if logger_configs is None:
+            raise ValueError('Logger %s (cruise %s) has no config declaration' %
+                             (logger_name, cruise_id))
 
-        # Is this mode the default mode for the cruise?
-        if mode_name == default_mode:
-          logging.info('    Setting %s as default mode', mode_name)
-          cruise.default_mode = mode
-          cruise.save()
+          # Find the corresponding configuration
+          for config_name in logger_configs:
+            config_spec = configs.get(config_name, None)
+            if config_spec is None:
+              raise ValueError('Config %s (declared by logger %s) not found' %
+                               (config_name, logger_name))
+            logging.info('  Associating config %s with logger %s',
+                         config_name, logger_name)
+            logging.info('config_spec: %s', config_spec)
 
-      ################
-      # Create the loggers
-      for logger_name, logger_spec in loggers.items():
-        logging.info('Creating logger %s (cruise %s)', logger_name, cruise_id)
-        logger = Logger(name=logger_name, cruise=cruise)
-        logger.save()
+            # A minor hack: fold the config's name into the spec
+            if not 'name' in config_spec:
+              config_spec['name'] = config_name
+            config = LoggerConfig(name=config_name, cruise=cruise,
+                                  logger=logger,
+                                  config_json=json.dumps(config_spec))
+            config.save()
+            # Is this logger config part of a mode?
+            for mode_name, mode_dict in modes.items():
+              logger_config_name = mode_dict.get(logger_name, None)
+              if logger_config_name and logger_config_name == config_name:
+                try:
+                  logging.debug('modes: %s',
+                                Mode.objects.filter(name=mode_name,
+                                                    cruise=cruise))
 
-        # Create and associate all relevant modes for the logger
-        logger_configs = logger_spec.get('configs', None)
-        if logger_configs is None:
-          raise ValueError('Logger %s (cruise %s) has no config declaration' %
-                           (logger_name, cruise_id))
+                  mode = Mode.objects.get(name=mode_name, cruise=cruise)
+                except Mode.DoesNotExist:
+                  raise ValueError('Mode %s does not exist?!?' % mode_name)
+                logging.info('    Associating config %s with mode %s',
+                             config_name, mode_name)
+                config.modes.add(mode)
 
-        # Find the corresponding configuration
-        for config_name in logger_configs:
-          config_spec = configs.get(config_name, None)
-          if config_spec is None:
-            raise ValueError('Config %s (declared by logger %s) not found' %
-                             (config_name, logger_name))
-          logging.info('  Associating config %s with logger %s',
-                       config_name, logger_name)
-          logging.info('config_spec: %s', config_spec)
+                # Is this config in the default mode of this logger?
+                if mode_name == default_mode:
+                  logging.info('    Setting logger %s to default config: %s',
+                               logger_name, config_name)
+                  logger.config = config
+                  logger.save()
 
-          # A minor hack: fold the config's name into the spec
-          if not 'name' in config_spec:
-            config_spec['name'] = config_name
-          config = LoggerConfig(name=config_name, cruise=cruise, logger=logger,
-                                config_json=json.dumps(config_spec))
-          config.save()
-          # Is this logger config part of a mode?
-          for mode_name, mode_dict in modes.items():
-            logger_config_name = mode_dict.get(logger_name, None)
-            if logger_config_name and logger_config_name == config_name:
-              try:
-                logging.debug('modes: %s',
-                              Mode.objects.filter(name=mode_name, cruise=cruise))
-
-                mode = Mode.objects.get(name=mode_name, cruise=cruise)
-              except Mode.DoesNotExist:
-                raise ValueError('Mode %s does not exist?!?' % mode_name)
-              logging.info('    Associating config %s with mode %s',
-                           config_name, mode_name)
-              config.modes.add(mode)
-
-              # Is this config in the default mode of this logger?
-              if mode_name == default_mode:
-                logging.info('    Setting logger %s to default config: %s',
-                             logger_name, config_name)
-                logger.config = config
-                logger.save()
-
-      logging.info('Cruise loaded - setting to default mode %s', default_mode)
-      self.set_active_mode(default_mode)
+        logging.info('Cruise loaded - setting to default mode %s', default_mode)
+        self.set_active_mode(default_mode)
 
     # Let anyone who's interested know that we've got new configurations.
     self.signal_load()
