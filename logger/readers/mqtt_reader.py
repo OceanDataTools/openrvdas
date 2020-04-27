3afc01631c4952854d4648d15cfea6be9a62a252
Pandelis Kirpoglou <pand181567@yahoo.com>
2020-04-27 20:25:11 +0300

    Initial commit of MQTT reader

---
+++ logger/readers/mqtt_reader.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+
+import json
+import logging
+import socket
+import sys
+
+from os.path import dirname, realpath; sys.path.append(dirname(dirname(dirname(realpath(__file__)))))
+
+from logger.utils.formats import Text
+from logger.readers.reader import Reader
+
+try:
+  import paho.mqtt.client as mqtt # import the client | $ pip installing paho-mqtt is necessary
+  PAHO_ENABLED = True
+except ModuleNotFoundError:
+  PAHO_ENABLED = False
+
+################################################################################
+class MQTTReader(Reader):
+  """
+   
+  """
+  def __init__(self, broker, channel):
+
+    super().__init__(output_format=Text)
+
+    if not PAHO_ENABLED:
+      raise ModuleNotFoundError('MQTTReader(): paho-mqtt is not installed. Please '
+                                'try "pip install paho-mqtt" prior to use.')
+    
+    self.broker = broker
+    self.channel = channel
+    
+    try:
+      self.paho = mqtt.Client('Instance1')
+
+      self.paho.connect(broker)
+      self.paho.subscribe(channel)
+
+    except mqtt.WebsocketConnectionError:
+      print('Unable to connect to broker at %s:%d',
+                    self.broker)
+
+  ############################
+def read(self):
+  while True:
+    try:
+      self.paho.loop_forever()
+      message = next(iter(self.paho.listen()))
+      logging.debug('Got message "%s"', message)
+      if message.get('type', None) == 'message':
+        data = message.get('data', None)
+        if data:
+          return data
+    except KeyboardInterrupt:
+      self.paho.disconnect()
+      exit(0)
+
+################################################################################
