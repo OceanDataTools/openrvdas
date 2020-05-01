66e66b2b62962b8d7060d996690d678134246c24
Pandelis Kirpoglou <pand181567@yahoo.com>
2020-05-01 13:44:13 +0300

    Change order of imports in mqtt test

--- logger/readers/test_mqtt_reader.py
+++ logger/readers/test_mqtt_reader.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 
 
-from logger.readers.mqtt_reader import MQTTReader
-# from logger.writers.mqtt_writer import MQTTWriter  // for when the writer is implemented
-
 import logging
 import subprocess
 import sys
@@ -14,6 +11,8 @@ import warnings
 from os.path import dirname, realpath
 sys.path.append(dirname(dirname(dirname(realpath(__file__)))))
 
+from logger.readers.mqtt_reader import MQTTReader
+# from logger.writers.mqtt_writer import MQTTWriter  // for when the writer is implemented
 
 try:
     # import the client | $ pip installing paho-mqtt is necessary
@@ -45,28 +44,28 @@ class TestMQTTReader(unittest.TestCase):
     def test_read(self):
         # Creating a new instance from the mqtt broker address above
         # try to connect
-    #writer
-     reader = MQTTReader(broker_address, channel)
+        # writer
+        reader = MQTTReader(broker_address, channel)
 
-     for i in range(len(SAMPLE_DATA)):
-      # writer.write(SAMPLE_DATA[i]) When the writer is implemented
-      self.assertEqual(SAMPLE_DATA[i], reader.read())
+        for i in range(len(SAMPLE_DATA)):
+            # writer.write(SAMPLE_DATA[i]) When the writer is implemented
+            self.assertEqual(SAMPLE_DATA[i], reader.read())
 
 
 ################################################################################
 if __name__ == '__main__':
-  import argparse
-  parser = argparse.ArgumentParser()
-  parser.add_argument('-v', '--verbosity', dest='verbosity',
-                      default=0, action='count',
-                      help='Increase output verbosity')
-  args = parser.parse_args()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbosity', dest='verbosity',
+                        default=0, action='count',
+                        help='Increase output verbosity')
+    args = parser.parse_args()
 
-  LOGGING_FORMAT = '%(asctime)-15s %(message)s'
-  logging.basicConfig(format=LOGGING_FORMAT)
+    LOGGING_FORMAT = '%(asctime)-15s %(message)s'
+    logging.basicConfig(format=LOGGING_FORMAT)
 
-  LOG_LEVELS ={0:logging.WARNING, 1:logging.INFO, 2:logging.DEBUG}
-  args.verbosity = min(args.verbosity, max(LOG_LEVELS))
-  logging.getLogger().setLevel(LOG_LEVELS[args.verbosity])
+    LOG_LEVELS = {0: logging.WARNING, 1: logging.INFO, 2: logging.DEBUG}
+    args.verbosity = min(args.verbosity, max(LOG_LEVELS))
+    logging.getLogger().setLevel(LOG_LEVELS[args.verbosity])
 
-  unittest.main(warnings='ignore')
+    unittest.main(warnings='ignore')
