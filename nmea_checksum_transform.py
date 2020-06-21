7f8d73efce1fb7f2565906dda4f1e92179da91cf
Anshika Agarwal <anshika99.agarwal@gmail.com>
2020-06-21 11:22:20 -0700

    NMEA Checksum Transform (#217)
    * Add files via upload
    
    test_nmea_checksum_transform.py is the unittest script for nmea_checksum_transform.py
    
    * Update nmea_checksum_transform.py
    
    * Update nmea_checksum_transform.py

---
+++ nmea_checksum_transform.py
@@ -0,0 +1,112 @@
+import logging
+import sys
+
+# For efficient checksum code
+from functools import reduce
+from operator import xor
+
+from logger.writers.writer import Writer
+
+############################
+
+def get_message_str(source):
+    """ Returns message_str, which is everything between the '$' and '*' in the source string """
+    
+    if ((source.find('$')==-1) or (source.find('*')==-1)):
+        return None
+    
+    start = source.index('$')+1
+    end = source.index('*')
+    message_str = source[start:end]
+    return message_str
+
+def get_checksum_value(source):
+    """ Returns checksum_value, which is the parsed checksum value (after '*') from the source string """
+    
+    if (source.find('*')==-1):
+        return None
+    
+    start = source.index('*')+1
+    checksum_value = source[start:]
+    return checksum_value
+
+def compute_checksum(source):
+    """Return hex checksum for source string."""
+    
+    return '%02X' % reduce(xor, (ord(c) for c in source))
+
+################################################################################
+class NMEAChecksumTransform:
+    """
+        NMEAChecksumTransform checks the integrity/completeness of a record by confirming whether or not the checksum matches. If the checksum matches, it returns the record, and otherwise it sends an error message.
+    """
+
+    DEFAULT_ERROR_MESSAGE = 'Bad checksum for record: '
+    def __init__(self, checksum_optional=False, error_message=DEFAULT_ERROR_MESSAGE, writer=None):
+        """
+            checksum_optional — if True, then pass record along even if checksum is missing
+            error_message — optional custom error message, if None then use DEFAULT_ERROR_MESSAGE
+            writer — optional error writer
+        """
+
+        self.checksum_optional = checksum_optional
+        self.error_message = error_message
+        self.writer = writer
+    
+        # Tries to utilize the write() method of writer, which it would only have if the object is a Writer. If this fails, writer is set to None.
+        try:
+            self.writer.write()
+        except AttributeError:
+            self.writer=None
+    
+    def transform(self, record):
+        """
+           Checks if computed checksum matches parsed checksum. If True, it returns the record, otherwise it calls send_error_message()
+           
+           record - the record in question
+        """
+        
+        if not record:
+            return None
+    
+        if not type(record) is str:
+            logging.warning('NMEAChecksumTransform passed non-string record '
+                        '(type %s): %s', type(record), record)
+            return None
+        
+        checksum_value = get_checksum_value(record)
+        
+        if (checksum_value == None):
+            if (self.checksum_optional):
+                return record
+        
+            #If here, checksum is not optional and does not exist
+            self.send_error_message(record, 'No checksum found in record ')
+            return None
+
+        message_str = get_message_str(record)
+        computed_checksum = compute_checksum(message_str)
+        
+        # If here, and we are about to see if it matches
+        if (computed_checksum == checksum_value):
+            return record
+        
+        # If here, then checksum exists but didn't match
+        self.send_error_message(record)
+        return None
+
+    def send_error_message(self, record, message=None):
+        """
+            Send error to writer if one exists, otherwise send it to stderr
+            
+            record - the record with the error
+            message - optional custom message. If None, then use self.error_message
+        """
+        
+        error_message = message or self.error_message
+        error_message += record
+        
+        if self.writer:
+            self.writer.write(error_message)
+        else:
+            logging.warning(error_message)
