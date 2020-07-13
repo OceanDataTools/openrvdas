a0e8c6f6db0433d9919e552f10746bfb69daa0c5
Anshika Agarwal <anshika99.agarwal@gmail.com>
2020-07-13 12:49:30 -0700

    Update test_cached_data_server.py

--- server/test_cached_data_server.py
+++ server/test_cached_data_server.py
@@ -24,6 +24,108 @@ class TestCachedDataServer(unittest.TestCase):
 
   ############################
   def test_basic(self):
+    WEBSOCKET_PORT = 8767
+    cds = CachedDataServer(port=WEBSOCKET_PORT)
+    cds.cache_record({'fields':{'field_1':'value_11',
+                                'field_2':'value_21',
+                                'field_3':'value_31'}})
+
+    # We call this in ensure_future, below
+    async def run_test():
+      async with websockets.connect('ws://localhost:%d' % WEBSOCKET_PORT) as ws:
+        now = time.time()
+
+        #####
+        to_send = {'type':'fields'}
+        await ws.send(json.dumps(to_send))
+        result = await ws.recv()
+        logging.info('got fields result: %s', result)
+        response = json.loads(result)
+        self.assertEqual(response.get('data', None),
+                         ['field_1', 'field_2', 'field_3'])
+        #####
+        to_send = {'type':'publish',
+                   'data':{'timestamp':time.time(),
+                           'fields':{'field_1':'value_12',
+                                     'field_2':'value_22'}}}
+        await ws.send(json.dumps(to_send))
+        result = await ws.recv()
+        logging.info('got publish result: %s', result)
+
+        #####
+        to_send = {'type':'subscribe', 'interval':0.2,
+                   'fields':{'field_1':{'seconds':1555507118},
+                             'field_2':{'seconds':0},
+                             'field_3':{'seconds':-1}}}
+        await ws.send(json.dumps(to_send))
+        result = await ws.recv()
+        logging.info('got subscribe result: %s', result)
+
+        #####
+        to_send = {'type':'ready'}
+        await ws.send(json.dumps(to_send))
+        await asyncio.sleep(0.1)
+        result = await ws.recv()
+        logging.info('got ready 1 result: %s', result)
+
+        response = json.loads(result)        
+        self.assertEqual(len(response['data']), 2)
+        self.assertEqual(len(response['data']['field_1']), 2)
+        self.assertEqual(response['data']['field_1'][0][1], 'value_11')
+        self.assertEqual(response['data']['field_1'][1][1], 'value_12')
+        self.assertEqual(len(response['data']['field_3']), 1)
+        self.assertEqual(response['data']['field_3'][0][1], 'value_31')
+
+        #####
+        to_send = {'type':'publish',
+                   'data':{'timestamp':time.time(),
+                           'fields':{'field_1':'value_13',
+                                     'field_2':'value_23'}}}
+        await ws.send(json.dumps(to_send))
+        await asyncio.sleep(0.1)
+        result = await ws.recv()
+        logging.info('got publish result: %s', result)
+
+        #####
+        to_send = {'type':'ready'}
+        await ws.send(json.dumps(to_send))
+        await asyncio.sleep(0.1)
+        result = await ws.recv()
+        logging.info('got ready 2 result: %s', result)
+
+        response = json.loads(result)        
+        self.assertEqual(len(response['data']), 2)
+        self.assertEqual(len(response['data']['field_1']), 1)
+        self.assertEqual(response['data']['field_1'][0][1], 'value_13')
+        self.assertEqual(len(response['data']['field_2']), 1)
+        self.assertEqual(response['data']['field_2'][0][1], 'value_23')
+
+        #####
+        to_send = {'type':'publish',
+                   'data':{'timestamp':time.time(),
+                           'fields':{'field_3':'value_33'}}}
+        await ws.send(json.dumps(to_send))
+        await asyncio.sleep(0.1)
+        result = await ws.recv()
+        logging.info('got publish result: %s', result)
+
+        #####
+        to_send = {'type':'ready'}
+        await ws.send(json.dumps(to_send))
+        await asyncio.sleep(0.1)
+        result = await ws.recv()
+        logging.info('got ready 3 result: %s', result)
+
+        response = json.loads(result)        
+        self.assertEqual(len(response['data']), 1)
+        self.assertEqual(len(response['data']['field_3']), 1)
+        self.assertEqual(response['data']['field_3'][0][1], 'value_33')
+
+    asyncio.new_event_loop().run_until_complete(run_test())
+    time.sleep(1)
+
+  ############################
+  def test_wildcard(self):
     WEBSOCKET_PORT = 8768
     cds = CachedDataServer(port=WEBSOCKET_PORT)
     cds.cache_record({'fields':{'field_1':'value_11',
@@ -58,8 +160,6 @@ class TestCachedDataServer(unittest.TestCase):
         #####
         to_send = {'type':'subscribe', 'interval':0.2,
                    'fields':{'field_*':{'seconds':1555507118}}}
-                       # 'field_2':{'seconds':0},
-                       # 'field_3':{'seconds':-1}}}
         await ws.send(json.dumps(to_send))
         result = await ws.recv()
         logging.info('got subscribe result: %s', result)
@@ -127,9 +227,10 @@ class TestCachedDataServer(unittest.TestCase):
     asyncio.new_event_loop().run_until_complete(run_test())
     time.sleep(1)
 
+
   ############################
   def test_disk_cache(self):
-    WEBSOCKET_PORT = 8767
+    WEBSOCKET_PORT = 8769
     tmpdir = tempfile.TemporaryDirectory()
     disk_cache = tmpdir.name + '/disk_cache'
     cds = CachedDataServer(port=WEBSOCKET_PORT, disk_cache=disk_cache)
