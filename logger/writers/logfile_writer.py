2731429b0abce7878f1f7b1554cf1dab59e6f53c
davidpablocohn <david.cohn@gmail.com>
2020-08-10 19:58:37 -0700

    Add use_system_time flag to LogfileWriter. Instead of looking for timestamp string at start of record, use system time to decide when to rotate logfiles.

--- logger/writers/logfile_writer.py
+++ logger/writers/logfile_writer.py
@@ -2,6 +2,7 @@
 
 import logging
 import sys
+import time
 from os.path import dirname, realpath; sys.path.append(dirname(dirname(dirname(realpath(__file__)))))
 
 from logger.utils import timestamp
@@ -15,10 +16,10 @@ class LogfileWriter(Writer):
   def __init__(self, filebase=None, flush=True,
                time_format=timestamp.TIME_FORMAT,
                date_format=timestamp.DATE_FORMAT,
+               use_system_time=False,
                split_char=' ', suffix='',
                rollover_hourly=False):
-    """
-    Write timestamped text records to file. Base filename will have
+    """Write timestamped text records to file. Base filename will have
     date appended, in keeping with R2R format recommendations
     (http://www.rvdata.us/operators/directory). When timestamped date on
     records rolls over to next day, create new file with new date suffix.
@@ -31,6 +32,10 @@ class LogfileWriter(Writer):
     date_fomat      A strftime-compatible string, such as '%Y-%m-%d';
                     defaults to whatever's defined in
                     utils.timestamps.DATE_FORMAT.
+
+    use_system_time If true, don't look for a timestamp at the start
+                    of the record; use the current system time (UTC)
+                    to decide when to roll over to the next file.
 
     split_char      Delimiter between timestamp and rest of message
 
@@ -46,6 +51,7 @@ class LogfileWriter(Writer):
     self.flush = flush
     self.time_format = time_format
     self.date_format = date_format
+    self.use_system_time = use_system_time
     self.split_char = split_char
     self.suffix = suffix
     self.rollover_hourly = rollover_hourly
@@ -68,21 +74,26 @@ class LogfileWriter(Writer):
         self.write(single_record)
       return
 
-    if not type(record) is str:
-      logging.error('LogfileWriter.write() - record is not timestamped string: '
-                    '%s', record)
+    # Get the timestamp we'll be using
+    if self.use_system_time:
+      ts = time.time()
+    elif not type(record) is str:
+      logging.error('LogfileWriter.write() - record is not timestamped and '
+                      'use_system_time not specified. String: %s', record)
       return
+    else:
+      try: # Try to extract timestamp from record
+        time_str = record.split(self.split_char)[0]
+        ts = timestamp.timestamp(time_str, time_format=self.time_format)
+      except ValueError:
+        logging.error('LogfileWriter.write() - bad timestamp: %s', record)
+        return
 
-    # First things first: get the date string from the record
-    try:
-      time_str = record.split(self.split_char)[0]
-      ts = timestamp.timestamp(time_str, time_format=self.time_format)
-      hr_str = self.rollover_hourly and timestamp.date_str(ts, date_format='_%H00') or ""      
-      date_str = timestamp.date_str(ts, date_format=self.date_format)
-      logging.debug('LogfileWriter date_str: %s', date_str)
-    except ValueError:
-      logging.error('LogfileWriter.write() - bad record timestamp: %s', record)
-      return
+    # Now parse ts into hour and date strings
+    hr_str = self.rollover_hourly and \
+             timestamp.date_str(ts, date_format='_%H00') or ""
+    date_str = timestamp.date_str(ts, date_format=self.date_format)
+    logging.debug('LogfileWriter date_str: %s', date_str)
 
     # Is it time to create a new file to write to?
     if not self.writer or date_str != self.current_date or hr_str != self.current_hour:
