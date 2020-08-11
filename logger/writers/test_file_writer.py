3428b3ac8d4f7758daf6b7f5bec33b6c36e84637
davidpablocohn <david.cohn@gmail.com>
2020-08-11 06:23:03 -0700

    Add generalized FileWriter

---
+++ logger/writers/test_file_writer.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+import logging
+import sys
+import tempfile
+import time
+import unittest
+import warnings
+
+from datetime import timezone
+
+from os.path import dirname, realpath; sys.path.append(dirname(dirname(dirname(realpath(__file__)))))
+
+from logger.utils.timestamp import time_str, DATE_FORMAT
+from logger.writers.file_writer import FileWriter
+
+SAMPLE_DATA = ['f1 line 1',
+               'f1 line 2',
+               'f1 line 3']
+
+
+########################################################
+class TestFileWriter(unittest.TestCase):
+
+  ############################
+  def test_write(self):
+    with tempfile.TemporaryDirectory() as tmpdirname:
+      writer = FileWriter(tmpdirname + '/f')
+      for line in SAMPLE_DATA:
+        writer.write(line)
+
+      with open(tmpdirname + '/f') as f:
+        for line in SAMPLE_DATA:
+          self.assertEqual(line, f.readline().strip())
+
+  ############################
+  def test_write_no_delimiter(self):
+    with tempfile.TemporaryDirectory() as tmpdirname:
+      writer = FileWriter(tmpdirname + '/f', delimiter=None)
+      for line in SAMPLE_DATA:
+        writer.write(line)
+
+      with open(tmpdirname + '/f') as f:
+        self.assertEqual(f.readline(), ''.join(SAMPLE_DATA))
+
+  ############################
+  def test_split_day(self):
+    """Test the split_by_date parameter, changing the date with each write."""
+    with tempfile.TemporaryDirectory() as tmpdirname:
+      writer = FileWriter(tmpdirname + '/g', split_by_time=True)
+
+      writer.timestamp = 1597150898
+      writer.write(SAMPLE_DATA[0])
+
+      writer.timestamp += 86400
+      writer.write(SAMPLE_DATA[1])
+
+      writer.timestamp += 86400
+      writer.write(SAMPLE_DATA[2])
+
+      logging.warning('ls %s', tmpdirname)
+
+      with open(tmpdirname + '/g-2020-08-11') as f:
+        self.assertEqual(SAMPLE_DATA[0], f.readline().strip())
+      with open(tmpdirname + '/g-2020-08-12') as f:
+        self.assertEqual(SAMPLE_DATA[1], f.readline().strip())
+      with open(tmpdirname + '/g-2020-08-13') as f:
+        self.assertEqual(SAMPLE_DATA[2], f.readline().strip())
+
+  ############################
+  def test_split_hour(self):
+    """Test the split_by_date parameter, changing the date with each write."""
+    with tempfile.TemporaryDirectory() as tmpdirname:
+      writer = FileWriter(tmpdirname + '/g', split_by_time=True,
+                          time_format='-%Y-%m-%d:%H')
+
+      writer.timestamp = 1597150898
+      writer.write(SAMPLE_DATA[0])
+
+      writer.timestamp += 3600
+      writer.write(SAMPLE_DATA[1])
+
+      writer.timestamp += 3600
+      writer.write(SAMPLE_DATA[2])
+
+      with open(tmpdirname + '/g-2020-08-11:13') as f:
+        self.assertEqual(SAMPLE_DATA[0], f.readline().strip())
+      with open(tmpdirname + '/g-2020-08-11:14') as f:
+        self.assertEqual(SAMPLE_DATA[1], f.readline().strip())
+      with open(tmpdirname + '/g-2020-08-11:15') as f:
+        self.assertEqual(SAMPLE_DATA[2], f.readline().strip())
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
