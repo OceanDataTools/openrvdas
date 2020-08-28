be33ce68573c3bab8336e82ecafeee2e24111777
davidpablocohn <david.cohn@gmail.com>
2020-08-28 16:20:40 -0700

    Fix InfluxDBWriter bailing out if it can't connect to InfluxDB on the first try. Resolves #241.

--- logger/writers/influxdb_writer.py
+++ logger/writers/influxdb_writer.py
@@ -33,18 +33,49 @@ import time
 ################################################################################
 class InfluxDBWriter(Writer):
   """Write to the specified file. If filename is empty, write to stdout."""
-  def __init__(self, bucket_name, measurement_name=None):
-    """
-    Write data records to the InfluxDB.
+  def __init__(self, bucket_name, measurement_name=None,
+               auth_token=INFLUXDB_AUTH_TOKEN,
+               org=INFLUXDB_ORG, url=INFLUXDB_URL):
+    """Write data records to the InfluxDB.
     ```
     bucket_name - the name of the bucket in InfluxDB.  If the bucket does
-                  not exists then this writer will try to create it.
+              not exists then this writer will try to create it.
 
     measurement_name - optional measurement name to use. If not provided,
-                  writer will use the record's data_id
+              writer will use the record's data_id.
+
+    auth_token - The auth token required by the InfluxDB instance. If omitted,
+              will look for value in imported INFLUXDB_AUTH_TOKEN and throw
+              an exception if it is not found.
+
+    org - The organization to associate with in the InfluxDB
+              instance. If omitted, will look for value in imported
+              INFLUXDB_ORG and throw an exception if it is not found.
+
+    url - The URL at which to connect with the InfluxDB instance. If
+              omitted, will look for value in imported INFLUXDB_ORG
+              and throw an exception if it is not found.
     ```
     """
     super().__init__(input_format=Text)
+    if not auth_token:
+      raise RuntimeError('No auth token specified in InfluxDBWriter and '
+                         'none found in database/settings.py. Have '
+                         'you copied over database/settings.py.dist '
+                         'to database/settings.py and followed the '
+                         'configuration instructions in it?')
+    if not org:
+      raise RuntimeError('No organization specified in InfluxDBWriter and '
+                         'none found in database/settings.py. Have '
+                         'you copied over database/settings.py.dist '
+                         'to database/settings.py and followed the '
+                         'configuration instructions in it?')
+    if not url:
+      raise RuntimeError('No URL specified in InfluxDBWriter and '
+                         'none found in database/settings.py. Have '
+                         'you copied over database/settings.py.dist '
+                         'to database/settings.py and followed the '
+                         'configuration instructions in it?')
 
     if not INFLUXDB_SETTINGS_FOUND:
       raise RuntimeError('File database/settings.py not found. '
@@ -57,43 +88,61 @@ class InfluxDBWriter(Writer):
                          'install using "pip install influxdb_client" prior '
                          'to using InfluxDBWriter.')
 
-    self.client = InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_AUTH_TOKEN, org=INFLUXDB_ORG)
-
+    self.auth_token = auth_token
+    self.org = org
+    self.url = url
     self.bucket_name = bucket_name
     self.measurement_name = measurement_name
+    self.write_api = None
 
-    # get the orgID from the name:
-    try:
-      self.organizations_api = self.client.organizations_api()
-      orgs = self.organizations_api.find_organizations()
-    except:
-      raise RuntimeError('Error connecting to the InfluxDB API. '
-                         'Please confirm that InfluxDB is running and '
-                         'that the authentication token is correct.')
+    # TODO: retry connecting if connection dies while writing.
+    self._connect()
 
-    our_org = next((org for org in orgs if org.name == INFLUXDB_ORG), None)
+  ############################
+  def _connect(self):
 
-    if not our_org:
-      raise RuntimeError('Can not find the organization "' + INFLUXDB_ORG + '" in InfluxDB')
+    while not self.write_api:
+      client = InfluxDBClient(url=self.url, token=self.auth_token, org=self.org)
 
-    self.org_id = our_org.id
-
-    # get the bucketID from the name:
-    self.bucket_api = self.client.buckets_api()
-    bucket = self.bucket_api.find_bucket_by_name(bucket_name)
-
-    # if the bucket does not exist then try to create it
-    if not bucket:
+      # get the orgID from the name:
       try:
-        new_bucket = self.bucket_api.create_bucket(bucket_name=bucket_name, org_id=self.org_id)
-        logging.info('Creating new bucket for: %s', bucket_name)
-        self.bucket_id = new_bucket.id
+        organizations_api = client.organizations_api()
+        orgs = organizations_api.find_organizations()
       except:
-        raise RuntimeError('Can not create bucket in InfluxDB for ' + bucket_name)
-    else:
-      self.bucket_id = bucket.id
+        self.client = None
+        logging.warning('Error connecting to the InfluxDB API. '
+                        'Please confirm that InfluxDB is running and '
+                        'that the authentication token is correct.'
+                        'Sleeping before trying again.')
+        time.sleep(5)
+        continue
+        
+      # Look up the organization id for our org
+      our_org = next((org for org in orgs if org.name == self.org), None)
+      if not our_org:
+        logging.fatal('Can not find org "%s" in InfluxDB', self.org)
+        raise RuntimeError('Can not find org "%s" in InfluxDB' % self.org)
+      self.org_id = our_org.id
 
-    self.write_api = self.client.write_api(write_options=ASYNCHRONOUS)
+      # get the bucketID from the name:
+      bucket_api = client.buckets_api()
+      bucket = bucket_api.find_bucket_by_name(self.bucket_name)
+
+      # if the bucket does not exist then try to create it
+      if bucket:
+        self.bucket_id = bucket.id
+      else:
+        try:
+          logging.info('Creating new bucket for: %s', self.bucket_name)
+          new_bucket = bucket_api.create_bucket(bucket_name=self.bucket_name,
+                                                org_id=self.org_id)
+          self.bucket_id = new_bucket.id
+        except:
+          logging.fatal('Can not create InfluxDB bucket "%s"', self.bucket_name)
+          raise RuntimeError('Can not create InfluxDB bucket "%s"'
+                             % self.bucket_name)
+
+      self.write_api = client.write_api(write_options=ASYNCHRONOUS)
 
   ############################
   def write(self, record):
