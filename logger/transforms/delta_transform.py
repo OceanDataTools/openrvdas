931fd7cbc6a5a76103f93749a6f880f896e64fa3
Anshika Agarwal <anshika99.agarwal@gmail.com>
2020-09-05 21:56:21 -0700

    Update delta_transform.py

--- logger/transforms/delta_transform.py
+++ logger/transforms/delta_transform.py
@@ -8,13 +8,10 @@ from logger.transforms.transform import Transform
 
 ################################################################################
 
+def polar_diff(last_value, value):
+  return ((value - last_value) + 180) % 360 - 180
+
 class DeltaTransform:
-
-  # Dict of {field_name: (previous_timestamp, previous_value)} pairs
-  last_value_dict = {}
-
-  def polar_diff(self, last_value, value):
-    return ((value - last_value) + 180) % 360 - 180
 
   def __init__(self, rate=False, field_type=None):
   """
@@ -24,6 +21,9 @@ class DeltaTransform:
       
     self.rate = rate
     self.field_type = field_type
+    
+    # Dict of {field_name: (previous_timestamp, previous_value)} pairs
+    self.last_value_dict = {}
   
   ############################
   def transform(self, record):
@@ -35,15 +35,11 @@ class DeltaTransform:
 
     if type(record) is DASRecord:
       fields = record.fields
-      timestamp = record.timestamp 
-    
-    elif type(record) is dict:
-      fields = record['fields']
-      timestamp = record['timestamp']
+      timestamp = record.timestamp
           
-      #elif type(record) is dict:
-      #fields = record.get('fields', None)
-      #timestamp = record.get('timestamp', None)
+    elif type(record) is dict:
+      fields = record.get('fields', None)
+      timestamp = record.get('timestamp', None)
       
     elif type(record) is list:
       results = []
@@ -52,39 +48,42 @@ class DeltaTransform:
       return results
     
     else:
-      return ('Record passed to DeltaTransform was neither a dict nor a '
-              'DASRecord. Type was %s: %s' % (type(record), str(record)[:80])) 
+      logging.info('Record passed to DeltaTransform was neither a dict nor a '
+              'DASRecord. Type was %s: %s' % (type(record), str(record)[:80]))
+      return None
     
-    if fields is None or timestamp is None:
-      return ('Record passed to DeltaTransform either does not have a field or '
-              'a timestamp')
+    if fields is None
+      logging.info('Record passed to DeltaTransform does not have "fields": %s', record)
+      return None
+      
+    if timestamp is None:
+      logging.info('Record passed to DeltaTransform does not have "timestamp": %s', record)
+      return None
     
     delta_values = {}
     rate_values = {}
     
     for key, value in fields.items():
+      if key in self.last_value_dict:
+        last_timestamp, last_value = self.last_value_dict.get(key, (None, None))
         
-      if key in DeltaTransform.last_value_dict:
-          
-        last_timestamp, last_value = DeltaTransform.last_value_dict.get(key, (None, None))
-            
         if self.field_type is None:
           delta_values[key] = value - last_value
         elif type(self.field_type) is dict:
           if key in self.field_type:
             if self.field_type[key] == 'polar':
-              delta_values[key] = DeltaTransform.polar_diff(self, last_value, value)
+              # delta_values[key] = DeltaTransform.polar_diff(self, last_value, value)
+              delta_values[key] = polar_diff(last_value, value)
           else:
             delta_values[key] = value - last_value
         else:
           return ('field_type passed to DeltaTransform is neither None nor a dict')
 
         rate_values[key] = delta_values[key] / (timestamp - last_timestamp)
-
-        DeltaTransform.last_value_dict[key] = (timestamp, value)
+        self.last_value_dict[key] = (timestamp, value)
 
       else:
-        DeltaTransform.last_value_dict[key] = (timestamp, value)
+        self.last_value_dict[key] = (timestamp, value)
         delta_values[key] = None
 
     if self.rate:
@@ -95,5 +94,4 @@ class DeltaTransform:
         results[field] = rate_values[field]
       return DASRecord(timestamp=timestamp, fields=results)
     else:
-
       return DASRecord(timestamp=timestamp, fields=delta_values)
