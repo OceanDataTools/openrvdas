f7d15fd2df6b714553a77bf35ee50d166c038969
Anshika Agarwal <anshika99.agarwal@gmail.com>
2020-09-05 22:59:16 -0700

    Update test_delta_transform.py

--- logger/transforms/test_delta_transform.py
+++ logger/transforms/test_delta_transform.py
@@ -8,7 +8,7 @@ from os.path import dirname, realpath; sys.path.append(dirname(dirname(dirname(r
 
 from logger.utils.das_record import DASRecord
 from logger.utils import timestamp
-from logger.transforms.delta_transform import DeltaTransform
+from logger.transforms.delta_transform import polar_diff, DeltaTransform
 
 class TestDeltaTransform(unittest.TestCase):
 
@@ -25,16 +25,41 @@ class TestDeltaTransform(unittest.TestCase):
         self.assertEqual(results.fields.get('gyroheading'), -8)
         self.assertEqual(results.fields.get('seatemp'), None)
 
+        t = DeltaTransform()
+        # First record, there's nothing to "delta" with
+        self.assertEqual(t.transform({'timestamp': 1, 'fields': {'variable': 15}}, None)
+
+        # Second record, at timestamp 2, has a delta of +5
+        self.assertEqual(
+          t.transform({'timestamp': 2, 'fields': {'variable': 20, 'variable2': 10}}),
+                      {'timestamp': 2, 'fields': {'variable': 5, 'variable2': None}})
+        # Third record, at timestamp 10, has a delta of -30
+        self.assertEqual(
+          t.transform({'timestamp': 10, 'fields': {'variable': -10, 'variable2': 15}}),
+                      {'timestamp': 10, 'fields': {'variable': -30, 'variable2': 5}})
 
     def test_polar_diff(self):
-        t = DeltaTransform()
+        self.assertEqual(polar_diff(5, 10), 5)
+        self.assertEqual(polar_diff(10, 5), -5)
+        self.assertEqual(polar_diff(5, 359), -6)
+        self.assertEqual(polar_diff(359, 5), 6)
+        self.assertEqual(polar_diff(90, 269), 179)
+        self.assertEqual(polar_diff(90, 271), -179)
+        
+        
+    def test_rate(self):
+        t = DeltaTransform(rate=True)
+        # First record, there's nothing to "delta" with
+        self.assertEqual(t.transform({'timestamp': 1, 'fields': {'variable': 15}}, None)
 
-        self.assertEqual(t.polar_diff(5, 10), 5)
-        self.assertEqual(t.polar_diff(10, 5), -5)
-        self.assertEqual(t.polar_diff(5, 359), -6)
-        self.assertEqual(t.polar_diff(359, 5), 6)
-        self.assertEqual(t.polar_diff(90, 269), 179)
-        self.assertEqual(t.polar_diff(90, 271), -179)
+        # Second record, at timestamp 2, has a delta of +5
+        self.assertEqual(
+          t.transform({'timestamp': 3, 'fields': {'variable': 20}}),
+                      {'timestamp': 3, 'fields': {'variable': 2.5}})
+        # Third record, at timestamp 10, has a delta of -30
+        self.assertEqual(
+          t.transform({'timestamp': 13, 'fields': {'variable': -10}}),
+                      {'timestamp': 13, 'fields': {'variable': -3}})
         
 
 if __name__ == '__main__':
