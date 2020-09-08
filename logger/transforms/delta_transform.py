dbaf232d2703fac2eabdec7fd5fd50a047556673
davidpablocohn <david.cohn@gmail.com>
2020-09-07 20:01:35 -0700

    Clean up DeltaTransform, add new tests to test_delta_transform.py

--- logger/transforms/delta_transform.py
+++ logger/transforms/delta_transform.py
@@ -6,6 +6,8 @@ from logger.utils.das_record import DASRecord
 from logger.utils import timestamp
 from logger.transforms.transform import Transform
 
+KNOWN_FIELD_TYPES = ['polar']
+
 ################################################################################
 def polar_diff(last_value, value):
   return ((value - last_value) + 180) % 360 - 180
@@ -13,22 +15,46 @@ def polar_diff(last_value, value):
 ################################################################################
 class DeltaTransform:
   def __init__(self, rate=False, field_type=None):
-  """
-    rate — whether to return the rate (delta/second) for some or all fields, or just return the delta
-    field_type — if not None, should be a dict specifying special field types, if any
-  """
-      
+    """Return a DASRecord (or dict, depending on input record type) with
+    each field's delta in value from it's previous value. If a field
+    is absent, it will be omitted. The first time a field appears, it
+    will be omitted (as there is no previous value to delta from). If
+    no deltas are available, None will be returned.
+
+    rate — If True, return the rate of change (delta/second). If a
+           list of field names, return rate of change for field names
+           in the list, simple delta for all others.  fields, or just
+           return the delta.
+
+    field_type — if not None, should be a dict mapping field names to
+           special field types, if any. Currently, only 'polar' is
+           implemented.
+    """
+    if not type(rate) in [bool, list]:
+      raise ValueError('"rate" argument in DeltaTransform must be either '
+                       'a list or Boolean. Found type %s' % type(rate))
+    if field_type:
+      if not type(field_type) is dict:
+        raise ValueError('"field_type" argument in DeltaTransform must be '
+                         ' either None or a dict. Found "%s"' %
+                         type(field_type))
+      # Check that any specified field types are ones we know about.
+      for field_name, this_field_type in field_type.items():
+        if not this_field_type in KNOWN_FIELD_TYPES:
+          raise ValueError('Unknown field_type specified for field %s: %s. '
+                           'Known field types are: %s' %
+                           (field_name, this_field_type, KNOWN_FIELD_TYPES))
     self.rate = rate
     self.field_type = field_type
-    
+
     # Dict of {field_name: (previous_timestamp, previous_value)} pairs
     self.last_value_dict = {}
-  
+
   ############################
   def transform(self, record):
     if not record:
       return None
-    
+
     fields = {}
 
     if type(record) is DASRecord:
@@ -38,58 +64,72 @@ class DeltaTransform:
     elif type(record) is dict:
       fields = record.get('fields', None)
       timestamp = record.get('timestamp', None)
-      
+
     elif type(record) is list:
       results = []
       for single_record in record:
        results.append(self.transform(single_record))
       return results
-    
+
     else:
       logging.info('Record passed to DeltaTransform was neither a dict nor a '
               'DASRecord. Type was %s: %s' % (type(record), str(record)[:80]))
       return None
-    
-    if fields is None
+
+    if fields is None:
       logging.info('Record passed to DeltaTransform does not have "fields": %s', record)
       return None
-      
+
     if timestamp is None:
       logging.info('Record passed to DeltaTransform does not have "timestamp": %s', record)
       return None
-    
+
     delta_values = {}
-    rate_values = {}
-    
+
     for key, value in fields.items():
-      if key in self.last_value_dict:
-        last_timestamp, last_value = self.last_value_dict.get(key, (None, None))
-        
-        if self.field_type is None:
-          delta_values[key] = value - last_value
-        elif type(self.field_type) is dict:
-          if key in self.field_type:
-            if self.field_type[key] == 'polar':
-              # delta_values[key] = DeltaTransform.polar_diff(self, last_value, value)
-              delta_values[key] = polar_diff(last_value, value)
-          else:
-            delta_values[key] = value - last_value
-        else:
-          return ('field_type passed to DeltaTransform is neither None nor a dict')
-
-        rate_values[key] = delta_values[key] / (timestamp - last_timestamp)
+      # If we don't have a previous value for this field, store the
+      # current one and move on to the next field.
+      if not key in self.last_value_dict:
         self.last_value_dict[key] = (timestamp, value)
+        continue
 
+      last_timestamp, last_value = self.last_value_dict.get(key, (None, None))
+
+      # Does this field have a special type?
+      if type(self.field_type) is dict:
+        this_field_type = self.field_type.get(key, None)
       else:
-        self.last_value_dict[key] = (timestamp, value)
-        delta_values[key] = None
+        this_field_type = self.field_type
 
-    if self.rate:
-      return DASRecord(timestamp=timestamp, fields=rate_values)
-    elif type(self.rate) is list:
-      results = {}
-      for field in self.rate:
-        results[field] = rate_values[field]
-      return DASRecord(timestamp=timestamp, fields=results)
-    else:
-      return DASRecord(timestamp=timestamp, fields=delta_values)
+      # What do we do with this field_type? 'None' is a simple diff
+      if this_field_type == 'polar':
+        delta_values[key] = polar_diff(last_value, value)
+      elif this_field_type is None:
+        delta_values[key] = value - last_value
+      else:
+        raise ValueError('DeltaTransform configured with unrecognized '
+                         'field type for %s: "%s"', key, this_field_type)
+
+      # Are we doing rate or simple diff for this field?
+      if self.rate is True or type(self.rate) is list and key in self.rate:
+        time_diff = timestamp - last_timestamp
+        # If rate, make sure it's a valid time difference. Bail if it isn't.
+        if time_diff <= 0:
+          logging.warning('Invalid difference in successive timestamps for '
+                          'field %s:  %g -> %g', key, last_timestamp, timestamp)
+          return None
+        delta_values[key] = delta_values[key] / time_diff
+
+      # Finally, save the current values for next time
+      self.last_value_dict[key] = (timestamp, value)
+
+    # If, at the end of it all, we don't have any fields, return None
+    if not delta_values:
+      return None
+
+    # If they gave us a dict, return a dict; if they gave us a
+    # DASRecord, return a DASRecord.
+    if type(record) is dict:
+      return {'timestamp': timestamp, 'fields': delta_values}
+
+    return DASRecord(timestamp=timestamp, fields=delta_values)
