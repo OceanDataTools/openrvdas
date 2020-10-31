e51eca9c4c6c66be539134a61913e5abac041b81
davidpablocohn <david.cohn@gmail.com>
2020-10-30 21:48:49 -0700

    Verified CentOS/Ubuntu Not yet tested on MacOS. Steps toward #245.

--- utils/install_openrvdas.sh
+++ utils/install_openrvdas.sh
@@ -268,29 +268,37 @@ function install_packages {
         export LD_RUN_PATH=/usr/local/lib
 
         # Fetch and build SQLite3
-        cd /tmp
         SQLITE_VERSION=3320300
-        SQLITE_BASE=sqlite-autoconf-${SQLITE_VERSION}
-        SQLITE_TGZ=${SQLITE_BASE}.tar.gz
-        [ -e $SQLITE_TGZ ] || wget https://www.sqlite.org/2020/${SQLITE_TGZ}
-        tar xzf ${SQLITE_TGZ}
-        cd ${SQLITE_BASE}
-        ./configure
-        make && make install
+        if [ `/usr/local/bin/sqlite3 --version |  cut -f1 -d' '` == '3.32.3' ]; then
+            echo Already have appropriate version of sqlite3
+        else
+            cd /tmp
+            SQLITE_BASE=sqlite-autoconf-${SQLITE_VERSION}
+            SQLITE_TGZ=${SQLITE_BASE}.tar.gz
+            [ -e $SQLITE_TGZ ] || wget https://www.sqlite.org/2020/${SQLITE_TGZ}
+            tar xzf ${SQLITE_TGZ}
+            cd ${SQLITE_BASE}
+            ./configure
+            make && make install
+        fi
 
-        # Build Python, too
-        cd /tmp
-        PYTHON_VERSION=3.8.3
-        PYTHON_BASE=Python-${PYTHON_VERSION}
-        PYTHON_TGZ=${PYTHON_BASE}.tgz
-        [ -e $PYTHON_TGZ ] || wget https://www.python.org/ftp/python/${PYTHON_VERSION}/${PYTHON_TGZ}
-        tar xvf ${PYTHON_TGZ}
-        cd ${PYTHON_BASE}
-        ./configure # --enable-optimizations
-        make altinstall
+        # Build Python, too, if we don't have right version
+        PYTHON_VERSION='3.8.3'
+        if [ "`/usr/local/bin/python3 --version`" == "Python $PYTHON_VERSION" ]; then
+            echo Already have appropriate version of Python3
+        else
+            cd /tmp
+            PYTHON_BASE=Python-${PYTHON_VERSION}
+            PYTHON_TGZ=${PYTHON_BASE}.tgz
+            [ -e $PYTHON_TGZ ] || wget https://www.python.org/ftp/python/${PYTHON_VERSION}/${PYTHON_TGZ}
+            tar xvf ${PYTHON_TGZ}
+            cd ${PYTHON_BASE}
+            ./configure # --enable-optimizations
+            make altinstall
 
-        ln -s -f /usr/local/bin/python3.8 /usr/local/bin/python3
-        ln -s -f /usr/local/bin/pip3.8 /usr/local/bin/pip3
+            ln -s -f /usr/local/bin/python3.8 /usr/local/bin/python3
+            ln -s -f /usr/local/bin/pip3.8 /usr/local/bin/pip3
+        fi
 
     # Ubuntu/Debian
     elif [ $OS_TYPE == 'Ubuntu' ]; then
