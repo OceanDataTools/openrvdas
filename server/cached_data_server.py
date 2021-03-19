876501b245b1284f1d6502e0b2fb4ed1334fe186
davidpablocohn <david.cohn@gmail.com>
2021-03-18 21:58:45 -0700

    Make CDS fail gracefully if can't write disk cache. Resolves #262

--- server/cached_data_server.py
+++ server/cached_data_server.py
@@ -91,6 +91,9 @@ class RecordCache:
         self.metadata = {}
         self.metadata_lock = threading.Lock()
 
+        # Disk files we've tried to write to but failed.
+        self.failed_files = set()
+
         # Create a lock for each key so threads don't step on each other
         self.locks = {key: threading.Lock() for key in self.keys()}
 
@@ -282,15 +285,33 @@ class RecordCache:
             return
 
         if not os.path.exists(disk_cache):
-            os.makedirs(disk_cache)
+            try:
+                os.makedirs(disk_cache)
+            except OSError as e:
+                logging.error('Unable to create disk cache directory "%s": %s', disk_cache, e)
+                return
 
         fields = self.keys()
         for field in fields:
+            disk_filename = disk_cache + '/' + field
+            if disk_filename in self.failed_files:
+                continue
             if field not in self.locks:
                 self.locks[field] = threading.Lock()
             with self.locks[field]:
-                with open(disk_cache + '/' + field, 'w') as cache_file:
-                    json.dump(self.data[field], cache_file)
+                try:
+                    with open(disk_filename, 'w') as cache_file:
+                        json.dump(self.data[field], cache_file)
+                except (PermissionError, IOError, OSError) as e:
+                    logging.warning('Unable to write disk cache file %s: %s', disk_filename, e)
+                    self.failed_files.add(disk_filename)
+
+                # This is BAD practice; but use it to figure out what else might go wrong
+                # so we can add it to specific exceptions above.s
+                except Exception as e:
+                    logging.warning('Unanticipated exception writing disk cache file %s: %s',
+                                    disk_filename, e)
+                    self.failed_files.add(disk_filename)
 
     ############################
     def load_from_disk(self, disk_cache):
