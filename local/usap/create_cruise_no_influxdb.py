574822f065b2765daf42136d71358d464331c7ac
NB Palmer IT Admin <it@nbp.usap.gov>
2021-08-01 13:37:27 +0000

    Update create_cruise_no_influxdb to use new 'skinny' logger structure.

--- local/usap/create_cruise_no_influxdb.py
+++ local/usap/create_cruise_no_influxdb.py
@@ -1,18 +1,24 @@
 #! /usr/bin/env python3
-"""This script creates a fairly simple "standard" cruise definition
-file from a port_def.yaml specification. A typical invocation would be
+"""This script creates a fairly simple "skinny" cruise definition
+file from a port_def.yaml specification that, in addition to other
+destinations, also writes parsed data to InfluxDB. A typical
+invocation would be
 
-  local/usap/create_cruise.py \
-    test/NBP1406/NBP1406_port_defs.yaml > test/NBP1406/NBP1406_cruise.yaml
+  local/usap/create_skinny_cruise.py \
+    test/NBP1406/NBP1406_port_defs.yaml > test/NBP1406/NBP1406_skinny_cruise.yaml
 
 It creates four modes:
   off      - nothing running
-  monitor  - write raw UDP to network and parsed to cached data server
-  log      - as above, but also write raw to file
-  log+db   - as above, but also write parsed to database
+  port     - like no_write (below), but only run subset of loggers
+  no_write - run all loggers, but don't write to disk
+  write    - as above, but also write raw to file
 
-It also includes a true_wind derived data logger that writes to the
-cached data server. There is no timeout checking of any loggers.
+All modes (except 'off') also write to the cached data server.
+
+Two derived data loggers are also included: true_wind and snapshot; these
+are written to the cached data server , but not disk.
+
+There is no timeout checking of any loggers.
 """
 import argparse
 import getpass
@@ -20,6 +26,8 @@ import logging
 import sys
 import time
 import yaml
+
+from collections import OrderedDict
 
 HEADER_TEMPLATE = """###########################################################
 ###########################################################
@@ -37,14 +45,85 @@ cruise:
   end: '%CRUISE_END%'
 """
 
-TRUE_WIND_TEMPLATE = """
+LOGGER_TEMPLATE = """  ########
+  %LOGGER%->off:
+    name: %LOGGER%->off
+
+  %LOGGER%->net:
+    name: %LOGGER%->net
+    readers:                    # Read from serial port
+    - class: SerialReader
+      kwargs:
+        baudrate: %BAUD%
+        port: %TTY%
+    transforms:                 # Add timestamp and logger label
+    - class: TimestampTransform
+    - class: PrefixTransform
+      kwargs:
+        prefix: %LOGGER%
+    writers:
+    - class: UDPWriter
+      kwargs:
+        port: %RAW_UDP_PORT%
+        destination: %UDP_DESTINATION%
+
+  %LOGGER%->net/file:
+    name: %LOGGER%->net/file
+    readers:                    # Read from serial port
+    - class: SerialReader
+      kwargs:
+        baudrate: %BAUD%
+        port: %TTY%
+    transforms:                 # Add timestamp
+    - class: TimestampTransform
+    writers:
+    - class: LogfileWriter      # Write to logfile
+      kwargs:
+        filebase: %FILE_ROOT%/%LOGGER%/raw/%CRUISE%_%LOGGER%
+    - class: ComposedWriter
+      kwargs:
+        transforms:
+        - class: PrefixTransform
+          kwargs:
+            prefix: %LOGGER%
+        writers:
+        - class: UDPWriter
+          kwargs:
+            port: %RAW_UDP_PORT%
+            destination: %UDP_DESTINATION%
+"""
+
+# Read raw records from UDP, parse them and distribute to CDS
+NET_READER_TEMPLATE = """  ########
+  net_reader->off:
+    name: net_reader->off
+
+  net_reader->on:
+    name: net_reader->on
+    readers:                    # Read from simulated serial port
+    - class: UDPReader
+      kwargs:
+        port: %RAW_UDP_PORT%
+    transforms:                 # Add timestamp and logger label
+    - class: ParseTransform
+      kwargs:
+        metadata_interval: 10
+        definition_path: %PARSE_DEFINITION_PATH%
+    writers:
+    - class: CachedDataWriter
+      kwargs:
+        data_server: %DATA_SERVER%
+
+"""
+
+TRUE_WIND_TEMPLATE = """  ########
   true_wind->off:
     name: true_wind->off
 
   true_wind->on:
     name: true_wind->on
     readers:
-      class: CachedDataReader
+    - class: CachedDataReader
       kwargs:
         data_server: %DATA_SERVER%
         subscription:
@@ -106,8 +185,9 @@ TRUE_WIND_TEMPLATE = """
         - class: CachedDataWriter
           kwargs:
             data_server: %DATA_SERVER%
+
 """
-SNAPSHOT_TEMPLATE = """
+SNAPSHOT_TEMPLATE = """  ########
   # Derived data subsampling logger
   snapshot->off:
     name: snapshot->off
@@ -115,7 +195,7 @@ SNAPSHOT_TEMPLATE = """
   snapshot->on:
     name: snapshot->on
     readers:
-      class: CachedDataReader
+    - class: CachedDataReader
       kwargs:
         data_server: %DATA_SERVER%
         subscription:
@@ -140,7 +220,6 @@ SNAPSHOT_TEMPLATE = """
               seconds: 0
             Grv1Value:
               seconds: 0
-
     transforms:
     - class: InterpolationTransform
       module: logger.transforms.interpolation_transform
@@ -203,150 +282,18 @@ SNAPSHOT_TEMPLATE = """
     - class: CachedDataWriter
       kwargs:
         data_server: %DATA_SERVER%
-"""
 
-OFF_TEMPLATE = """
-  %LOGGER%->off:
-    name: %LOGGER%->off
-"""
-
-NET_WRITER_TEMPLATE = """
-  %LOGGER%->net:
-    name: %LOGGER%->net
-    readers:                    # Read from simulated serial port
-      class: SerialReader
-      kwargs:
-        baudrate: %BAUD%
-        port: %TTY%
-    transforms:                 # Add timestamp and logger label
-    - class: TimestampTransform
-    - class: PrefixTransform
-      kwargs:
-        prefix: %LOGGER%
-    writers:
-    - class: UDPWriter
-      kwargs:
-        port: %RAW_UDP_PORT%
-        destination: %UDP_DESTINATION%
-    - class: ComposedWriter     # Also parse to fields and send to CACHE UDP
-      kwargs:                   # port for CachedDataServer to pick up
-        transforms:
-        - class: ParseTransform
-          kwargs:
-            metadata_interval: 10
-            definition_path: %PARSE_DEFINITION_PATH%
-        writers:
-        - class: CachedDataWriter
-          kwargs:
-            data_server: %DATA_SERVER%
-"""
-
-FILE_NET_WRITER_TEMPLATE = """
-  %LOGGER%->file/net:
-    name: %LOGGER%->file/net
-    readers:                    # Read from simulated serial port
-      class: SerialReader
-      kwargs:
-        baudrate: %BAUD%
-        port: %TTY%
-    transforms:                 # Add timestamp
-    - class: TimestampTransform
-    writers:
-    - class: LogfileWriter      # Write to logfile
-      kwargs:
-        filebase: %FILE_ROOT%/%LOGGER%/raw/%CRUISE%_%LOGGER%
-    - class: ComposedWriter     # Also prefix with logger name and broadcast
-      kwargs:                   # raw NMEA on UDP
-        transforms:
-        - class: PrefixTransform
-          kwargs:
-            prefix: %LOGGER%
-        writers:
-        - class: UDPWriter
-          kwargs:
-            port: %RAW_UDP_PORT%
-            destination: %UDP_DESTINATION%
-    - class: ComposedWriter     # Also parse to fields and send to CACHE UDP
-      kwargs:                   # port for CachedDataServer to pick up
-        transforms:
-        - class: PrefixTransform
-          kwargs:
-            prefix: %LOGGER%
-        - class: ParseTransform
-          kwargs:
-            metadata_interval: 10
-            definition_path: %PARSE_DEFINITION_PATH%
-        writers:
-        - class: CachedDataWriter
-          kwargs:
-            data_server: %DATA_SERVER%
-"""
-
-FULL_WRITER_TEMPLATE = """
-  %LOGGER%->file/net/db:
-    name: %LOGGER%->file/net/db
-    readers:                    # Read from simulated serial port
-      class: SerialReader
-      kwargs:
-        baudrate: %BAUD%
-        port: %TTY%
-    transforms:                 # Add timestamp
-    - class: TimestampTransform
-    writers:
-    - class: LogfileWriter      # Write to logfile
-      kwargs:
-        filebase: %FILE_ROOT%/%LOGGER%/raw/%CRUISE%_%LOGGER%
-    - class: ComposedWriter     # Also prefix with logger name and broadcast
-      kwargs:                   # raw NMEA on UDP
-        transforms:
-        - class: PrefixTransform
-          kwargs:
-            prefix: %LOGGER%
-        writers:
-        - class: UDPWriter
-          kwargs:
-            port: %RAW_UDP_PORT%
-            destination: %UDP_DESTINATION%
-    - class: ComposedWriter     # Also parse to fields and send to CACHE UDP
-      kwargs:                   # port for CachedDataServer to pick up
-        transforms:
-        - class: PrefixTransform
-          kwargs:
-            prefix: %LOGGER%
-        - class: ParseTransform
-          kwargs:
-            metadata_interval: 10
-            definition_path: %PARSE_DEFINITION_PATH%
-        writers:
-        - class: CachedDataWriter
-          kwargs:
-            data_server: %DATA_SERVER%
-    - class: ComposedWriter     # Also write parsed data to database
-      kwargs:
-        transforms:
-        - class: PrefixTransform
-          kwargs:
-            prefix: %LOGGER%
-        - class: ParseTransform
-          kwargs:
-            metadata_interval: 10
-            definition_path: %PARSE_DEFINITION_PATH%
-        writers:
-        - class: DatabaseWriter
 """
 
 ####################
-
-
 def fill_substitutions(template, substitutions):
-    output = template
-    for src, dest in substitutions.items():
-        output = output.replace(str(src), str(dest))
-    return output
+  output = template
+  for src, dest in substitutions.items():
+    output = output.replace(str(src), str(dest))
+  return output
 
 ################################################################################
 ################################################################################
-
 
 parser = argparse.ArgumentParser()
 parser.add_argument('def_filename', metavar='def_filename', type=str,
@@ -354,33 +301,33 @@ parser.add_argument('def_filename', metavar='def_filename', type=str,
 args = parser.parse_args()
 
 with open(args.def_filename, 'r') as fp:
-    try:
-        port_def = yaml.load(fp, Loader=yaml.FullLoader)
-    except AttributeError:
-        # If they've got an older yaml, it may not have FullLoader)
-        port_def = yaml.load(fp)
+  try:
+    port_def = yaml.load(fp, Loader=yaml.FullLoader)
+  except AttributeError:
+    # If they've got an older yaml, it may not have FullLoader)
+    port_def = yaml.load(fp)
 
 # Create dict of variables we're going to substitute into the templates
 substitutions = {
-    '%CRUISE%': port_def.get('cruise', {}).get('id'),
-    '%CRUISE_START%': port_def.get('cruise', {}).get('start'),
-    '%CRUISE_END%': port_def.get('cruise', {}).get('end'),
+  '%CRUISE%': port_def.get('cruise', {}).get('id'),
+  '%CRUISE_START%': port_def.get('cruise', {}).get('start'),
+  '%CRUISE_END%': port_def.get('cruise', {}).get('end'),
 
-    '%UDP_DESTINATION%': port_def.get('network', {}).get('destination', '255.255.255.255'),
-    '%RAW_UDP_PORT%': port_def.get('network', {}).get('raw_udp_port'),
-    '%PARSED_UDP_PORT%': port_def.get('network', {}).get('parsed_udp_port'),
-    '%DATA_SERVER%': port_def.get('network', {}).get('data_server'),
+  '%UDP_DESTINATION%': port_def.get('network', {}).get('destination', '255.255.255.255'),
+  '%RAW_UDP_PORT%': port_def.get('network', {}).get('raw_udp_port'),
+  '%PARSED_UDP_PORT%': port_def.get('network', {}).get('parsed_udp_port'),
+  '%DATA_SERVER%': port_def.get('network', {}).get('data_server'),
 
-    '%FILE_ROOT%': port_def.get('file_root', '/var/tmp/log'),
+  '%FILE_ROOT%': port_def.get('file_root', '/var/tmp/log'),
 
-    '%PARSE_DEFINITION_PATH%':  port_def.get('parse_definition_path', ''),
+  '%PARSE_DEFINITION_PATH%':  port_def.get('parse_definition_path', ''),
 
-    '%COMMAND_LINE%': ' '.join(sys.argv),
-    '%DATE_TIME%': time.asctime(time.gmtime()),
-    '%USER%': getpass.getuser(),
+  '%COMMAND_LINE%': ' '.join(sys.argv),
+  '%DATE_TIME%': time.asctime(time.gmtime()),
+  '%USER%': getpass.getuser(),
 }
 
-loggers = port_def.get('ports').keys()
+loggers =  port_def.get('ports').keys()
 
 ################################################################################
 # Start with header template
@@ -397,11 +344,16 @@ LOGGER_DEF = """  %LOGGER%:
     configs:
     - %LOGGER%->off
     - %LOGGER%->net
-    - %LOGGER%->file/net
-    - %LOGGER%->file/net/db
+    - %LOGGER%->net/file
 """
 for logger in loggers:
-    output += fill_substitutions(LOGGER_DEF, substitutions).replace('%LOGGER%', logger)
+  output += fill_substitutions(LOGGER_DEF, substitutions).replace('%LOGGER%', logger)
+
+output += """  net_reader:
+    configs:
+    - net_reader->off
+    - net_reader->on
+"""
 output += """  true_wind:
     configs:
     - true_wind->off
@@ -421,34 +373,28 @@ modes:
   'off':
 """
 for logger in loggers:
-    output += '    %LOGGER%: %LOGGER%->off\n'.replace('%LOGGER%', logger)
+  output += '    %LOGGER%: %LOGGER%->off\n'.replace('%LOGGER%', logger)
+output += '    net_reader: net_reader->off\n'
 output += '    true_wind: true_wind->off\n'
 output += '    snapshot: snapshot->off\n'
 
-# monitor
+#### no_write
 output += """
-  monitor:
+  no_write:
 """
 for logger in loggers:
-    output += '    %LOGGER%: %LOGGER%->net\n'.replace('%LOGGER%', logger)
+  output += '    %LOGGER%: %LOGGER%->net\n'.replace('%LOGGER%', logger)
+output += '    net_reader: net_reader->on\n'
 output += '    true_wind: true_wind->on\n'
 output += '    snapshot: snapshot->on\n'
 
-# log
+#### write
 output += """
-  log:
+  write:
 """
 for logger in loggers:
-    output += '    %LOGGER%: %LOGGER%->file/net\n'.replace('%LOGGER%', logger)
-output += '    true_wind: true_wind->on\n'
-output += '    snapshot: snapshot->on\n'
-
-# log+db
-output += """
-  'log+db':
-"""
-for logger in loggers:
-    output += '    %LOGGER%: %LOGGER%->file/net/db\n'.replace('%LOGGER%', logger)
+  output += '    %LOGGER%: %LOGGER%->net/file\n'.replace('%LOGGER%', logger)
+output += '    net_reader: net_reader->on\n'
 output += '    true_wind: true_wind->on\n'
 output += '    snapshot: snapshot->on\n'
 
@@ -464,40 +410,22 @@ output += """
 configs:
 """
 for logger in loggers:
-    output += """  ########"""
-    output += fill_substitutions(OFF_TEMPLATE, substitutions).replace('%LOGGER%', logger)
-    # Special case for true winds, which is a derived logger
 
-    # Look up port.tab values for this logger
-    if logger not in loggers:
-        logging.warning('No port.tab entry found for %s; skipping...', logger)
-        continue
+  logger_port_def = port_def.get('ports').get(logger).get('port_tab')
+  if not logger_port_def:
+    logging.warning('No port def for %s', logger)
 
-    logger_port_def = port_def.get('ports').get(logger).get('port_tab')
-    if not logger_port_def:
-        logging.warning('No port def for %s', logger)
+  (inst, tty, baud, datab, stopb, parity, igncr, icrnl, eol, onlcr,
+   ocrnl, icanon, vmin, vtime, vintr, vquit, opost) = logger_port_def.split()
+  logger_def = fill_substitutions(LOGGER_TEMPLATE, substitutions)
+  logger_def = logger_def.replace('%LOGGER%', logger)
+  logger_def = logger_def.replace('%TTY%', tty)
+  logger_def = logger_def.replace('%BAUD%', baud)
+  output += logger_def
 
-    (inst, tty, baud, datab, stopb, parity, igncr, icrnl, eol, onlcr,
-     ocrnl, icanon, vmin, vtime, vintr, vquit, opost) = logger_port_def.split()
-    net_writer = fill_substitutions(NET_WRITER_TEMPLATE, substitutions)
-    net_writer = net_writer.replace('%LOGGER%', logger)
-    net_writer = net_writer.replace('%TTY%', tty)
-    net_writer = net_writer.replace('%BAUD%', baud)
-    output += net_writer
+output += fill_substitutions(NET_READER_TEMPLATE, substitutions)
 
-    file_net_writer = fill_substitutions(FILE_NET_WRITER_TEMPLATE, substitutions)
-    file_net_writer = file_net_writer.replace('%LOGGER%', logger)
-    file_net_writer = file_net_writer.replace('%TTY%', tty)
-    file_net_writer = file_net_writer.replace('%BAUD%', baud)
-    output += file_net_writer
-
-    full_writer = fill_substitutions(FULL_WRITER_TEMPLATE, substitutions)
-    full_writer = full_writer.replace('%LOGGER%', logger)
-    full_writer = full_writer.replace('%TTY%', tty)
-    full_writer = full_writer.replace('%BAUD%', baud)
-    output += full_writer
-
-# Add in the true wind configurations
+# Add in the true wind and snapshot configurations
 output += fill_substitutions(TRUE_WIND_TEMPLATE, substitutions)
 output += fill_substitutions(SNAPSHOT_TEMPLATE, substitutions)
 
