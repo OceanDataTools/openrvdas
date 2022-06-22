86d0f3fe6e9208ce472e71777614ae1ec656386f
davidpablocohn <david.cohn@gmail.com>
2022-06-22 15:28:02 -0700

    fix broken Influx install

--- utils/install_influxdb.sh
+++ utils/install_influxdb.sh
@@ -276,7 +276,7 @@ enabled = 1
 gpgcheck = 1
 gpgkey = https://repos.influxdata.com/influxdb.key
 EOF
-        sudo yum install -y influxdb2 influxdb2-cli
+        sudo yum install -y influxdb2-cli
 
     elif [ $OS_TYPE == 'Ubuntu' ]; then
         # From https://portal.influxdata.com/downloads/
