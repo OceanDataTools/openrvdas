aa27b0014a5bdd5523ad6e85a477cd396203b080
davidpablocohn <david.cohn@gmail.com>
2022-06-27 12:33:38 -0700

    First pass update

--- utils/install_openrvdas.sh
+++ utils/install_openrvdas.sh
@@ -175,6 +175,9 @@ function set_default_variables {
     DEFAULT_INSTALL_FIREWALLD=no
     DEFAULT_OPENRVDAS_AUTOSTART=yes
 
+    DEFAULT_INSTALL_SIMULATE_NBP=no
+    DEFAULT_RUN_SIMULATE_NBP=no
+
     DEFAULT_SUPERVISORD_WEBINTERFACE=no
     DEFAULT_SUPERVISORD_WEBINTERFACE_AUTH=no
     DEFAULT_SUPERVISORD_WEBINTERFACE_PORT=9001
@@ -216,6 +219,9 @@ DEFAULT_RVDAS_USER=$RVDAS_USER
 DEFAULT_INSTALL_MYSQL=$INSTALL_MYSQL
 DEFAULT_INSTALL_FIREWALLD=$INSTALL_FIREWALLD
 DEFAULT_OPENRVDAS_AUTOSTART=$OPENRVDAS_AUTOSTART
+
+DEFAULT_INSTALL_SIMULATE_NBP=$INSTALL_SIMULATE_NBP
+DEFAULT_RUN_SIMULATE_NBP=$RUN_SIMULATE_NBP
 
 DEFAULT_SUPERVISORD_WEBINTERFACE=$SUPERVISORD_WEBINTERFACE
 DEFAULT_SUPERVISORD_WEBINTERFACE_AUTH=$SUPERVISORD_WEBINTERFACE_AUTH
@@ -944,6 +950,17 @@ function setup_supervisor {
         AUTOSTART=false
     fi
 
+    # Whether the simulation script is commented out, and if not,
+    # whether it should autorun on boot
+    SIMULATE_NBP_COMMENT=';'
+    AUTOSTART_SIMULATE_NBP='false'
+    if [ $INSTALL_SIMULATE_NBP = 'yes' ]; then
+        SIMULATE_NBP_COMMENT=''
+    fi
+    if [ $RUN_SIMULATE_NBP = 'yes' ]; then
+        AUTOSTART_SIMULATE_NBP='true'
+    fi
+
     # MacOS
     if [ $OS_TYPE == 'MacOS' ]; then
         ETC_HOME=/usr/local/etc
@@ -1054,17 +1071,17 @@ stderr_logfile_maxbytes=10000000 ; 10M
 stderr_logfile_maxbackups=100
 user=$RVDAS_USER
 
-[program:simulate_nbp]
-command=${VENV_BIN}/python logger/utils/simulate_data.py --config test/NBP1406/simulate_NBP1406.yaml
-directory=${INSTALL_ROOT}/openrvdas
-autostart=false
-autorestart=true
-startretries=3
-killasgroup=true
-stderr_logfile=/var/log/openrvdas/simulate_nbp.stderr
-stderr_logfile_maxbytes=10000000 ; 10M
-stderr_logfile_maxbackups=100
-user=$RVDAS_USER
+${SIMULATE_NBP_COMMENT}[program:simulate_nbp]
+${SIMULATE_NBP_COMMENT}command=${VENV_BIN}/python logger/utils/simulate_data.py --config test/NBP1406/simulate_NBP1406.yaml
+${SIMULATE_NBP_COMMENT}directory=${INSTALL_ROOT}/openrvdas
+${SIMULATE_NBP_COMMENT}autostart=false
+${SIMULATE_NBP_COMMENT}autorestart=${AUTOSTART_SIMULATE_NBP}
+${SIMULATE_NBP_COMMENT}startretries=3
+${SIMULATE_NBP_COMMENT}killasgroup=true
+${SIMULATE_NBP_COMMENT}stderr_logfile=/var/log/openrvdas/simulate_nbp.stderr
+${SIMULATE_NBP_COMMENT}stderr_logfile_maxbytes=10000000 ; 10M
+${SIMULATE_NBP_COMMENT}stderr_logfile_maxbackups=100
+${SIMULATE_NBP_COMMENT}user=$RVDAS_USER
 
 [group:web]
 programs=nginx,uwsgi
@@ -1072,8 +1089,8 @@ programs=nginx,uwsgi
 [group:openrvdas]
 programs=logger_manager,cached_data_server
 
-[group:simulate]
-programs=simulate_nbp
+${SIMULATE_NBP_COMMENT}[group:simulate]
+${SIMULATE_NBP_COMMENT}programs=simulate_nbp
 EOF
 
     # Copy supervisor file into place
@@ -1370,6 +1387,24 @@ echo "and starting processes logger_manager and cached_data_server."
 echo
 yes_no "Start the OpenRVDAS server on boot? " $DEFAULT_OPENRVDAS_AUTOSTART
 OPENRVDAS_AUTOSTART=$YES_NO_RESULT
+
+# Set up simulate_nbp script?
+echo
+echo "#####################################################################"
+echo "For test installations, OpenRVDAS can configure simulated inputs from"
+echo "stored data, which will allow you to run the \"NBP1406_cruise.yaml\""
+echo "configuration out of the box. This script will be configured to run under"
+echo "supervisord as \"simulate:simulate_nbp\"."
+echo
+yes_no "Do you want to install this script?"
+INSTALL_SIMULATE_NBP=$YES_NO_RESULT  DEFAULT_INSTALL_SIMULATE_NBP
+
+if [ $INSTALL_SIMULATE_NBP == 'yes' ]; then
+  yes_no "Run InfluxDB on boot?" $DEFAULT_RUN_SIMULATE_NBP
+  RUN_SIMULATE_NBP=$YES_NO_RESULT
+else
+  RUN_SIMULATE_NBP=no
+fi
 
 #########################################################################
 # Enable Supervisor web-interface?
