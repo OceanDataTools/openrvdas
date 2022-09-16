523142500e3d0b58679c4e849d8d0e0f94c00e2b
davidpablocohn <david.cohn@gmail.com>
2022-09-16 16:55:34 -0700

    typo

--- utils/install_influxdb.sh
+++ utils/install_influxdb.sh
@@ -535,7 +535,7 @@ EOF
         fi
 
         if [[ $USE_SSL == 'yes' ]];then
-            INFLUX_SSL_OPTIONS="--tls-cert=\"SSL_CRT_LOCATION\" --tls-key=\"SSL_KEY_LOCATION\""
+            INFLUX_SSL_OPTIONS="--tls-cert=\"$SSL_CRT_LOCATION\" --tls-key=\"$SSL_KEY_LOCATION\""
         else
             INFLUX_SSL_OPTIONS=""
         fi
