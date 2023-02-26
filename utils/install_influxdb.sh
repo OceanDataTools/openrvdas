27391f7b3405b57ed4e117b72d2c1897f469e647
davidpablocohn <david.cohn@gmail.com>
2023-02-26 08:47:03 -0800

    Resolves #320

--- utils/install_influxdb.sh
+++ utils/install_influxdb.sh
@@ -324,7 +324,7 @@ EOF
         fi
     elif [ $OS_TYPE == 'Ubuntu' ]; then
         # From https://portal.influxdata.com/downloads/
-        wget -qO- https://repos.influxdata.com/influxdb.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdb.gpg > /dev/null
+        wget -qO- https://repos.influxdata.com/influxdata-archive_compat.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdb.gpg > /dev/null
         export DISTRIB_ID=$(lsb_release -si); export DISTRIB_CODENAME=$(lsb_release -sc)
         echo "deb [signed-by=/etc/apt/trusted.gpg.d/influxdb.gpg] https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list > /dev/null
         sudo apt-get update
