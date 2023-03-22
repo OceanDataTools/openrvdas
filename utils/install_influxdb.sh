8288c19cd80fc2251bf2bcabd0f34863979aa2db
davidpablocohn <david.cohn@gmail.com>
2023-03-21 22:23:21 -0300

    debug influxdb installation

--- utils/install_influxdb.sh
+++ utils/install_influxdb.sh
@@ -403,9 +403,6 @@ EOF
     elif [ $OS_TYPE == 'Ubuntu' ]; then
         sudo apt-get install -y apt-transport-https
         sudo apt-get install -y software-properties-common wget
-        #wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
-        #echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
-        #sudo apt-get update
         sudo apt-get install grafana
     fi
 
@@ -466,11 +463,6 @@ EOF
 
     # If we're on Ubuntu
     elif [ $OS_TYPE == 'Ubuntu' ]; then
-        # influxdb.key GPG Fingerprint: 05CE15085FC09D18E99EFB22684A14CF2582E0C5
-        #wget -q https://repos.influxdata.com/influxdata-archive_compat.key
-        #echo '23a1c8836f0afc5ed24e0486339d7cc8f6790b83886c4c96995b88a061c5bb5d influxdata-archive_compat.key' | sha256sum -c && cat influxdata-archive_compat.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdb.gpg > /dev/null
-        #echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdb.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
-        #sudo apt-get update
         sudo apt-get install telegraf
     fi
 
