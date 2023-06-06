8ea7517d22004a3a4e88f066cd41e843e56b3c55
KPed <nobody@example.com>
2023-06-06 01:44:59 +0000

    whittling down on namespace pollution

--- sqlite_gui/index.html.js
+++ sqlite_gui/index.html.js
@@ -35,26 +35,28 @@
 //  have contact with the server or not.
 // 
 ////////////////////////////////////////////////////////////////////
-(function() {
+    
+var time_td = (function() {
     var el = document.getElementById('time_td');
     if (!el) { return; }
-    // default timeout 
+    // default timeout if no value in config file
+    // FIXME:  This actually evaluates before the JSON is loaded,
+    //         so the config file basically does nothing.
     var t = 1000;
-    if (odas) {
-        if (odas.Timeouts) {
-            t = odas.Timeouts.Now * 1000;
-        }
-    }
 
     function update_now() {
+        if (!el) { return; }
         el.innerHTML = Date().substring(0, 24);
     }
-    // Even if we don't have a time_td on whatever page this
-    // gets loaded on, this does little harm.
     var interval = setInterval(update_now, t);
  
     // We could probably wait one second to write the time, but...
     update_now();
+
+    return {
+        el: el,
+    }
+        
 })();
 
 ////////////////////////////////////////////////////////////////////
@@ -180,27 +182,198 @@ function process_message(message_str) {
     }
 }
 
-function status_cruise_definition(timestamp, config) {
-    if (timestamp < global_last_cruise_timestamp) {
-        // stale update.  ignore.
-        return;
-    }
-    // console.debug("cruise_def_updated()");
-    global_last_cruise_timestamp = timestamp;
-    var new_fields = update_cruise_definition(timestamp, config);
-    var el =  document.getElementById('empty_loggers');
-    var sal = document.getElementById('status_and_loggers');
-    // Show/hide empty_logger, status_and_loggers
-    if (config.loggers) {
-        el.className = "d-none";
-        sal.className = "d-block";
-    } else {
-        el.className = "d-block";
-        sal.className = "d-none";
-    }
-    return new_fields;
-}
+var CruiseDef = (function() {
+    var our_timestamp = 0;
+    var el_empty =  document.getElementById('empty_loggers');
+    var el_populated = document.getElementById('status_and_loggers');
+    var loggers = {}
 
+    function diff_loggers(config_loggers) {
+        var new_loggers = [];   // new loggers we don't have
+        var old_loggers = [];    // loggers we had that are gone
+
+        // Hmmm. performance wise, it is faster to do it this way?
+        var new_set = new Set(Object.keys(config_loggers));
+        var old_set = new Set(Object.keys(loggers));
+
+        for (var logger of new_set) {
+            if (old_set.has(logger)) {
+                continue;
+            }
+            new_loggers.push(logger);
+        }
+        for (logger of old_set) {
+            if (new_set.has(logger)) {
+                continue;
+            }
+            old_loggers.push(logger);
+        }
+        return {
+            new: new_loggers,
+            old: old_loggers,
+            size: new_loggers.length + old_loggers.length,
+        }
+    }
+
+    function show_correct_divs(config) {
+        // Interwebs says using the classes works better
+        // than setting visibility on mobile.  May be true.
+        if (config.loggers) {
+            el_empty.className = "d-none";
+            el_populated.className = "d-block";
+        } else {
+            el_empty.className = "d-block";
+            el_populated.className = "d-none";
+        }
+    }
+
+    function update_cruise_id(cruise_id, filename) {
+        var el = document.getElementById('cruise_id');
+        if (el) {
+            txtNode = el.childNodes[0];
+            txtNode.nodeValue = cruise_id;
+            // Add tooltip with definition file name
+            // cruise_definition.filename
+        }
+        // FIXME;  For now, we're doing this.  Yuk!
+        el = document.getElementById('conf_file');
+        if (el) {
+            el.innerHTML = filename;
+        }
+    }
+
+    function update_logger_rows(loggers) {
+
+    }
+
+    // Called when a message of type "status:cruise_definition"
+    // arrives on the websocket
+    var status_message = function(timestamp, config) {
+        if (timestamp < our_timestamp) {
+            console.debug('Stale status:cruise_definition message');
+            return;
+        }
+        our_timestamp = timestamp;
+        show_correct_divs(config);
+        // var new_fields = update_cruise_definition(timestamp, config);
+        // Ensure all the correct parts are there
+        if (!config.modes) {
+            console.log('Cruise definition has no modes - ignoring.');
+            return {};
+        }
+        if (!config.active_mode) {
+            console.log('Cruise definition has no active mode - ignoring.');
+            return {};
+        }
+        if (!config.loggers) {
+            console.log('Cruise definition has no loggers - ignoring.');
+            return {};
+        }
+        // update cruise name on navbar
+        if (config.cruise_id) {
+            update_cruise_id(config.cruise_id, config.filename);
+        }
+        // Set the cruise mode button to the active mode
+        active_mode = config.active_mode;
+        cruise_mode.update(active_mode);
+        // get diff between old loggers and config.loggers
+        // We could just add the new ones and delete the old ones,
+        // but that would change the order.  I think we care...
+        var diff = diff_loggers(config.loggers);
+        loggers = config.loggers;
+        console.info('Loaded new configuration');
+        if (diff.size == 0) {
+           // Set the logger button texts
+           // FIXME:  I think we already have a function for this.
+           for (var logger_name in loggers) {
+                var button = document.getElementById(logger_name + '_btn');
+                if (button) {
+                    button.innerHTML = loggers[logger_name].active;
+                }
+            }
+            return {};
+        } else {
+            update_logger_rows(loggers);
+        }
+        var foo = 'bar';
+        // If stuff has gotten updated, need to update CDS fields to
+        // subscribe to.
+        var new_fields = {};
+
+        // Update logger_manager status, resubscribe to updates asking for greater
+        // of last hour of records or last 100 records.
+        // var button = document.getElementById('logger_manager_mode_button');
+        //button.innerHTML = global_active_mode;
+        var time_window = {'seconds':1*60*60, 'back_records': 100};
+        new_fields['stderr:logger_manager'] = time_window;
+
+        ////////////////////////////////
+        // If the list of loggers has changed, we need to rebuild the
+        // list. Begin by emptying out table rows except for header row.
+        var table = document.getElementById('logger_table_body');
+        try {
+            var row_count = table.rows.length;
+            var keep_first_num_rows = 3;
+            for (var i = keep_first_num_rows; i < row_count; i++) {
+                table.deleteRow(keep_first_num_rows);
+            }
+        } catch {}
+
+        // Stash our new logger list in the globals, then update the
+        // loggers, creating one new row for each.
+        for (var logger_name in loggers) {
+            //console.log('setting up logger ' + logger_name);
+            var logger = loggers[logger_name];
+
+            // table row creation
+            var tr = document.createElement('tr');
+            tr.setAttribute('id', logger_name + '_row');
+
+            var config_td = document.createElement('td');
+            config_td.setAttribute('id', logger_name + '_config_td');
+
+            var button = LoggerButton.create(logger_name, logger.active);
+            config_td.appendChild(button);
+            tr.appendChild(config_td);
+
+            var stderr_td = document.createElement('td');
+            var stderr_div = document.createElement('div');
+
+            stderr_div.setAttribute('id', logger_name + '_stderr');
+            stderr_div.className = 'stderr_window';
+            stderr_td.appendChild(stderr_div);
+            tr.appendChild(stderr_td);
+            table.appendChild(tr);
+
+            // Also, since we now have a new logger, we'll want to
+            // subscribe to stderr updates for it.  Send each logger
+            // window with at greatest of one hour past log messges or
+            // most recent 100 messages
+            var name = 'stderr:logger:' + logger_name
+            new_fields[name] = time_window;
+            new_fields['stderr:logger:' + logger_name] = {'seconds':6*60*60, 'back_records': 100};
+        }
+        //console.log('New fields are: ' + JSON.stringify(new_fields));
+        //////////////////
+        // Finally: Do we have new field subscriptions? Create the
+        // subscription request.  But make sure we keep listening for future
+        // logger_list, mode_list mode updates.
+        // FIXME:  Are new_fields used anywhere besides cruise_definition?
+        if (Object.keys(new_fields).length) {
+            new_fields['status:cruise_definition'] = {'seconds':-1};
+            new_fields['status:cruise_mode'] = {'seconds':-1};
+            new_fields['status:logger_status'] = {'seconds':-1};
+            new_fields['status:file_update'] = {'seconds':0};
+            var subscribe_message = {'type':'subscribe', 'fields':new_fields};
+            WS.write(subscribe_message);
+        }
+    }
+        
+    return {
+        status_message: status_message,
+    }   
+
+})()
 
 ////////////////////////////
 ////////////////////////////
@@ -236,10 +409,11 @@ function process_data_message(message) {
         switch (field_name) {
             // Cruise definition updatee
             case 'status:cruise_definition':
-                var nf = status_cruise_definition(timestamp, values);
-                for (var key in nf) {
-                    new_fields[key] = nf[key];
-                }
+                var nf = CruiseDef.status_message(timestamp, values);
+                // var nf = status_cruise_definition(timestamp, values);
+                // for (var key in nf) {
+                //    new_fields[key] = nf[key];
+                // }
                 break;
 
             // Cruise Mode update
@@ -291,162 +465,6 @@ function process_data_message(message) {
         }
 
     }
-}
-
-////////////////////////////
-// Process a cruise_definition update
-function update_cruise_definition(timestamp, cruise_definition) {
-    // Utility function used to determine if we need to update
-    // console.log("update_cruise_def: ", JSON.stringify(cruise_definition))
-    // FIXME:  Set()? Return a diff: (added, removed) = diff(a, b)
-    function array_keys_match(a, b) {
-        if (Object.keys(a).length != Object.keys(b).length) {
-            return false;
-        }
-        for (var key in a) {
-            if (b[key] == undefined) {
-                return false;
-            }  
-        }
-        return true;
-    }
-
-    // Make sure this definition has all the parts we need
-    if (!cruise_definition.modes) {
-        console.log('Cruise definition has no modes - ignoring.');
-        return {};
-    }
-    if (!cruise_definition.active_mode) {
-        console.log('Cruise definition has no active mode - ignoring.');
-        return {};
-    }
-    if (!cruise_definition.loggers) {
-        console.log('Cruise definition has no loggers - ignoring.');
-        return {};
-    }
-    // make cruise_definition available globally
-    odas.cruise_definition = cruise_definition;
-
-  ////////////////////////////////
-  // Update the cruise id and filename
-  //document.getElementById('cruise_id').innerHTML = cruise_definition.cruise_id;
-
-    // update cruise name on navbar
-    // FIXME:  this should be a dropdown with "reload/load/etc"
-    if (cruise_definition.cruise_id) {
-        var el = document.getElementById('cruise_id');
-        if (el) {
-            txtNode = el.childNodes[0];
-            txtNode.nodeValue = cruise_definition.cruise_id;
-            // Add tooltip with definition file name
-            // cruise_definition.filename
-            
-        }
-        el = document.getElementById('conf_file');
-        if (el) {
-            el.innerHTML = cruise_definition.filename;
-        }
-    }  
-
-    ////////////////////////////////
-    // Now update the cruise modes
-    // var modes = cruise_definition.modes;
-    //cruise_mode.update(global_active_mode);
-    //global_active_mode = cruise_definition.active_mode;
-    //var mode_button = document.getElementById('mode_button');
-    //mode_button.innerHTML = global_active_mode;
-
-    ////////////////////////////////
-    // Now update loggers
-    var loggers = cruise_definition.loggers;
-
-    // Has the actual list of loggers changed? If not, only update
-    // what has changed.
-    if (array_keys_match(global_loggers, loggers)) {
-        for (var logger_name in loggers) {
-//            var button = document.getElementById(logger_name + '_config_button');
-            var button = document.getElementById(logger_name + '_btn');
-            button.innerHTML = loggers[logger_name].active;
-        }
-        return {};
-    } // what do we do if they haven't changed?
-
-    // If stuff has gotten updated, need to update CDS fields to
-    // subscribe to.
-    var new_fields = {};
-
-    // Update logger_manager status, resubscribe to updates asking for greater
-    // of last hour of records or last 100 records.
-    cruise_mode.update(global_active_mode);
-    // var button = document.getElementById('logger_manager_mode_button');
-    //button.innerHTML = global_active_mode;
-    new_fields['stderr:logger_manager'] = {'seconds':60*60, 'back_records':100};
-
-    ////////////////////////////////
-    // If the list of loggers has changed, we need to rebuild the
-    // list. Begin by emptying out table rows except for header row.
-    var table = document.getElementById('logger_table_body');
-    try {
-        var row_count = table.rows.length;
-        var keep_first_num_rows = 3;
-        for (var i = keep_first_num_rows; i < row_count; i++) {
-            table.deleteRow(keep_first_num_rows);
-        }
-    } catch {}
-
-
-    // Stash our new logger list in the globals, then update the
-    // loggers, creating one new row for each.
-    global_loggers = loggers;
-    for (var logger_name in loggers) {
-        //console.log('setting up logger ' + logger_name);
-        var logger = loggers[logger_name];
-
-        // table row creation
-        var tr = document.createElement('tr');
-        tr.setAttribute('id', logger_name + '_row');
-
-        var config_td = document.createElement('td');
-        config_td.setAttribute('id', logger_name + '_config_td');
-
-        var button = LoggerButton.create(logger_name, logger.active);
-        config_td.appendChild(button);
-        tr.appendChild(config_td);
-
-        var stderr_td = document.createElement('td');
-        var stderr_div = document.createElement('div');
-
-        stderr_div.setAttribute('id', logger_name + '_stderr');
-        stderr_div.className = 'stderr_window';
-        stderr_td.appendChild(stderr_div);
-        tr.appendChild(stderr_td);
-        table.appendChild(tr);
-
-        // Also, since we now have a new logger, we'll want to
-        // subscribe to stderr updates for it.  Send each logger
-        // window with at greatest of one hour past log messges or
-        // most recent 100 messages
-        var name = 'stderr:logger:' + logger_name
-        var time_window = {'seconds':1*60*60, 'back_records': 100};
-        new_fields[name] = time_window;
-        new_fields['stderr:logger:' + logger_name] = {'seconds':6*60*60, 'back_records': 100};
-    }
-    console.debug('Loaded new cruise.');
-    //console.log('New fields are: ' + JSON.stringify(new_fields));
-    //////////////////
-    // Finally: Do we have new field subscriptions? Create the
-    // subscription request.  But make sure we keep listening for future
-    // logger_list, mode_list mode updates.
-    // FIXME:  Are new_fields used anywhere besides cruise_definition?
-    if (Object.keys(new_fields).length) {
-        new_fields['status:cruise_definition'] = {'seconds':-1};
-        new_fields['status:cruise_mode'] = {'seconds':-1};
-        new_fields['status:logger_status'] = {'seconds':-1};
-        new_fields['status:file_update'] = {'seconds':0};
-        var subscribe_message = {'type':'subscribe', 'fields':new_fields};
-        WS.write(subscribe_message);
-    }
-    return new_fields; // why?
 }
 
 
