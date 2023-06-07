9cd6a4f5ac84126c4a81048fcc7eae545d82db76
KPed <nobody@example.com>
2023-06-07 21:04:53 +0000

    type datetime, not timestamp

--- sqlite_gui/CreateSQLiteDatabase.sh
+++ sqlite_gui/CreateSQLiteDatabase.sh
@@ -2,8 +2,8 @@
 
 rm -f openrvdas.sql
 
-sqlite3 openrvdas.sql 'CREATE TABLE Cruise (highlander integer primary key not null, config blob, [loaded_time] timestamp, compressed integer);'
-sqlite3 openrvdas.sql 'CREATE TABLE lastupdate (highlander integer primary key not null, [timestamp] timestamp);'
+sqlite3 openrvdas.sql 'CREATE TABLE Cruise (highlander integer primary key not null, config blob, [loaded_time] datetime, compressed integer);'
+sqlite3 openrvdas.sql 'CREATE TABLE lastupdate (highlander integer primary key not null, [timestamp] datetime);'
 sqlite3 openrvdas.sql 'INSERT INTO lastupdate (highlander, timestamp) VALUES (1, CURRENT_TIMESTAMP);'
-sqlite3 openrvdas.sql 'CREATE TABLE logmessages (timestamp timestamp primary key not null, loglevel integer, cruise text, source text, user text, message text);'
+sqlite3 openrvdas.sql 'CREATE TABLE logmessages (timestamp datetime primary key not null, loglevel integer, cruise text, source text, user text, message text);'
 
