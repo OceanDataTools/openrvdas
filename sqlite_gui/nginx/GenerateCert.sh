e15b7ea815c16bfe1024d76c81bde50fa6a1f175
KPed <nobody@example.com>
2023-06-17 18:16:43 +0000

    incremental towards final

--- sqlite_gui/nginx/GenerateCert.sh
+++ sqlite_gui/nginx/GenerateCert.sh
@@ -2,6 +2,7 @@
 
 # Quick and dirty script to generaate a cert/key pair for nginx.
 
+exec 2> debug.log
 
 QUERY='fields=status,message,countryCode,regionName,city,lat,lon'
 curl -s "http://ip-api.com/json/?${QUERY}" > ip-api.com
@@ -53,14 +54,15 @@ SERVER_IP_1=${IP}
 SERVER_IP_2=127.0.0.1
 SERVER_IP_3=127.0.1.1
 
-## Script to generate a SAN Cert
-
+# Script will generate a SAN enabled wildcard Cert
 function generate_self_signed_ca() {
     echo "Generaing domain key"
-    openssl genrsa -out ${DOMAIN}.key 4096 2>/dev/null
+    echo "Generaing domain key" >&2
+    openssl genrsa -out ${DOMAIN}.key 4096
     STATUS=$?
     if [ $STATUS != 0 ] ; then
         echo "Error creating domain key"
+        echo "See debug.log for more details"
         return
     fi
 
@@ -68,34 +70,43 @@ function generate_self_signed_ca() {
     openssl req -x509 -new -nodes -sha512 -days 3653 \
     -subj "/C=${C}/ST=${ST}/L=${L}/O=${O}/OU=${OU}/CN=${COMMON_NAME}" \
     -key ${DOMAIN}.key \
-    -out ${DOMAIN}.crt 2>/dev/null
+    -out ${DOMAIN}.crt
     STATUS=$?
     if [ $STATUS != 0 ] ; then
         echo "Error creating domain cert"
+        echo "See debug.log for more details"
     fi
 }
 
 function generate_server_certificate() {
     echo "Generaing server key"
-    openssl genrsa -out openrvdas.key 4096 2>/dev/null
+    echo >&2
+    echo "Generaing server key" >&2
+    openssl genrsa -out openrvdas.key 4096 
     STATUS=$?
     if [ $STATUS != 0 ] ; then
         echo "Error creating server key"
+        echo "See debug.log for more details"
         return
     fi
 
     echo "Generaing Certificate Signing Request"
+    echo >&2
+    echo "Generaing Certificate Signing Request" >&2
     openssl req -sha512 -new \
         -subj "/C=${C}/ST=${ST}/L=${L}/O=${O}/OU=${OU}/CN=${COMMON_NAME}" \
         -key openrvdas.key \
-        -out openrvdas.csr 2>/dev/null
+        -out openrvdas.csr
     STATUS=$?
     if [ $STATUS != 0 ] ; then
         echo "Error creating CSR"
+        echo "See debug.log for more details"
         return
     fi
 
 # For future consideration, consider OCSP, too
+    echo >&2
+    echo "Creating v3 extentions file" >&2
 cat > v3.ext <<-EOF
 authorityKeyIdentifier=keyid,issuer
 basicConstraints=CA:FALSE
@@ -112,14 +123,18 @@ IP.2=${SERVER_IP_2}
 IP.3=${SERVER_IP_3}
 EOF
 
+    echo "Signing request"
+    echo >&2
+    echo "Signing request" >&2
     openssl x509 -req -sha512 -days 3653 \
         -extfile v3.ext \
         -CA ${DOMAIN}.crt -CAkey ${DOMAIN}.key -CAcreateserial \
         -in openrvdas.csr \
-        -out openrvdas.cert 2>/dev/null
+        -out openrvdas.cert
     STATUS=$?
     if [ $STATUS != 0 ] ; then
         echo "Error signing request"
+        echo "See debug.log for more details"
         return
     fi
     rm -f ${DOMAIN}.srl
@@ -127,10 +142,14 @@ EOF
     rm -f v3.ext
 
 
+    echo "Converting PEM to crt"
+    echo >&2
+    echo "Converting PEM to crt" >&2
     openssl x509 -inform PEM -in openrvdas.cert -out openrvdas.crt
     STATUS=$?
     if [ $STATUS != 0 ] ; then
         echo "Error converting PEM to cert"
+        echo "See debug.log for more details"
         return
     fi
     rm -f openrvdas.cert
@@ -143,14 +162,69 @@ function copy_certificates_to_destination() {
     cp ${CERTS_DIR}/${FILE_NAME}.key ${CERT_DIR}
 }
 
-function update_linux_cert_store() {
-    # This is for debian-style distros
-    cp ${CERTS_DIR}/${FILE_NAME}.crt /usr/local/share/ca-certificates
-    update-ca-certificates
+function update_cert_store() {
+    # I think we should be ruuning as a user, but
+    # if we are running as root or script is running sudo, do not sudo
+    # SUDO=''
+    # if ${UID} != 0 SUDO='/usr/bin/sudo'
+
+    # Get OS flavor, ask if cannot determine
+    determine_flavor
+    [ -n ${OS_TYPE} ] || ask_os_type
+
+    if [ $OS_TYPE == 'Ubuntu' ] ; then
+        DEST=/usr/local/share/ca-certificates
+        ${SUDO} /bin/cp ${DOMAIN}.crt ${DEST}
+        ${SUDO} update-ca-certificates  # what's the path?
+    fi
+
+    if [ $OS_TYPE == 'CentOS' ] ; then
+        DEST=/etc/pki/ca-trust/source/anchors
+        ${SUDO} /bin/cp ${DOMAIN}.crt ${DEST}
+        ${SUDO} /usr/bin/update-ca-trust
+    fi
+
+    if [ $OS_TYPE == 'Darwin' ] ; then
+        # Darwin (note: no way to test this on my laptop):
+        KEYCHAINS='/System/Library/Keychains'
+        ROOTCERTS='SystemRootCertificates.keychain'
+        KEYCHAIN="${KEYCHAINS}/${ROOTCERTS}"
+        ${SUDO} security add-trusted-cert -k ${KEYCHAIN} ${DOMAIN}.crt
+    fi
 }
+
+function ask_os_type {
+    declare -A allowed
+    allowed[CentOS]=CentOS
+    allowed[Ubuntu]=Ubuntu
+    allowed[Darwin]=Darwin
+    echo "Cannot determine the OS Type.  Please select"
+    while [ -z ${OS_TYPE} ] ; do
+        read -p "(CentOS, Ubuntu, Darwin): " reply
+        [ -z "${reply}" ] && reply="Argabarg"   # Blank indexes bad
+        [[ ${allowed[$reply]+_} ]] && OS_TYPE=$reply
+    done
+}
+
+function determine_flavor {
+    # We don't need to check versions because they're already
+    # running OpenRVDAS.  So just get the flavor.
+    if [ `uname -s` == 'Darwin' ] ; then
+        OS_TYPE=MacOS
+        return
+    fi
+    LIKE=`grep -i "id_like" /etc/os-release`
+    # This will work on Fedora, Rocky, RHEL, CentOS, etc.
+    [[ ${LIKE} =~ 'rhel' ]] && OS_TYPE='CentOS'
+    # This will work on debian, ubuntu, RaspiOS, etc...
+    [[ ${LIKE} =~ 'debian' ]] && OS_TYPE='Ubuntu'
+    # SUSE/OpenSUSE say "suse" in the id_like
+}
+
 
 generate_self_signed_ca
 generate_server_certificate
-#copy_certificates_to_destination
-
+# echo "some pithy prompt"
+# read reply until reply in yes/no
+# if yes, 'update_cert_store'
 
