c44eec97089caf4e81fa4657a22aa459459b8990
davidpablocohn <david.cohn@gmail.com>
2023-10-11 19:51:24 -0700

    Undo grafana chown

--- utils/install_influxdb.sh
+++ utils/install_influxdb.sh
@@ -436,7 +436,7 @@ EOF
         wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
         echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
         sudo apt-get update
-        sudo apt-get install grafana
+        sudo apt-get install -y grafana
 
     fi
 
