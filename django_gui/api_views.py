4f50265f65168cc6a635a1bbe35948f1ce2ad028
Gabriel Sargeant <gabe.sargeant@gmail.com>
2024-03-31 21:16:52 +1100

    wip

--- django_gui/api_views.py
+++ django_gui/api_views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import Group, User
 from rest_framework import permissions, viewsets,  status, views
 from rest_framework.response import Response
 from django.http import Http404
+from django.shortcuts import get_object_or_404
 
 #RVDAS Models + Serializers
 from django_gui.models import Cruise, Logger
@@ -13,40 +14,60 @@ from django_gui.serializers import CruiseSerializer, LoggerSerializer
 #     API endpoint that allows Cruise to be viewed or edited.
 #     """
 #     queryset = Cruise.objects.all().order_by('-id')
-#     serializer_class = CruiseSerializer
+#     
 #     permission_classes = [permissions.IsAuthenticated]
 
 
 class CruiseDetailView(viewsets.ModelViewSet):
     """
-    API endpoint that allows updating and deleting a specific MyModel instance.
+    API endpoint that allows updating and deleting a Model instance.
     """
-    queryset = Cruise.objects.all().order_by('-id')
     serializer_class = CruiseSerializer
-
-    def get_object(self, pk):
-        try:
-            return Cruise.objects.get(pk=pk)
-        except Cruise.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        instance = self.get_object(pk)
-        serializer = CruiseSerializer(instance)
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def list(self, request):
+        queryset = Cruise.objects.all().order_by('-id')
+        self.queryset = queryset
+        serializer = CruiseSerializer(queryset, many=True)
         return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        instance = self.get_object(pk)
-        serializer = CruiseSerializer(instance, data=request.data)
+    
+    def retrieve(self, request, pk=None):
+        queryset = Cruise.objects.all()
+        cruise = get_object_or_404(queryset, pk=pk)
+        serializer = CruiseSerializer(cruise)
+        return Response(serializer.data)
+    
+    def create(self, request):
+        serializer = CruiseSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def update(self, request, pk=None):
+        cruise = Cruise.objects.get(pk=pk)
+        serializer = CruiseSerializer(cruise, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        instance = self.get_object(pk)
-        instance.delete()
+    
+    def partial_update(self, request, pk=None):
+        cruise = Cruise.objects.get(pk=pk)
+        serializer = CruiseSerializer(cruise, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def destroy(self, request, pk=None):
+        try:
+            cruise = Cruise.objects.get(pk=pk)
+        except Cruise.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        cruise.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class LoggerViewSet(viewsets.ModelViewSet):
     """
