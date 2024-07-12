2c3f23ffdb01bea249f164fc0605101e640ca2b8
davidpablocohn <david.cohn@gmail.com>
2024-07-12 06:34:30 -0700

    FIPSify install_influxdb. #394

--- utils/install_influxdb.sh
+++ utils/install_influxdb.sh
@@ -351,8 +351,8 @@ EOF
         if [ $OS_VERSION == '7' ]; then
             sudo yum install -y influxdb2 influxdb2-cli
         else
-            dnf download influxdb
-            rpm -ivh --nofiledigest ‘influxdb2*’
+            dnf download influxdb influxdb2-cli
+            rpm -ivh --nofiledigest influxdb2 influxdb2-cli
             #sudo yum install -y --nobest influxdb2 influxdb2-cli
         fi
     elif [ $OS_TYPE == 'Ubuntu' ]; then
