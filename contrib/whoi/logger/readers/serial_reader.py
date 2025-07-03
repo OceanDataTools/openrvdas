a09daff2e4a0cb1422cb910645219d424d1c5629
Webb Pinner <webbpinner@gmail.com>
2025-07-02 22:59:05 -0400

    Adds serial reader with WHOI extensions
    Adds a serial reader implementation with extensions
    specifically for WHOI, including prefixing messages
    with a timestamp and configurable encoding options.
    Includes unit tests for the new reader.

---
+++ contrib/whoi/logger/readers/serial_reader.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+
+import logging
+import sys
+
+from os.path import dirname, realpath
+sys.path.append(dirname(dirname(dirname(dirname(dirname(realpath(__file__)))))))
+from logger.utils import timestamp  # noqa: E402
+from logger.readers.serial_reader import SerialReader as Reader # noqa: E402
+
+LOGGING_TIME_FORMAT = '%Y-%m-%d %H:%M:%S'  # ISO 8601
+TIMEZONE = timestamp.timezone.utc
+
+################################################################################
+class SerialReader(Reader):
+    """
+    Read records from a serial port.
+    """
+
+    def __init__(self,  port, baudrate=9600, bytesize=8, parity='N',
+                 stopbits=1, timeout=None, xonxoff=False, rtscts=False,
+                 write_timeout=None, dsrdtr=False, inter_byte_timeout=None,
+                 exclusive=None, max_bytes=None, eol=None,
+                 encoding='utf-8', encoding_errors='ignore',
+                 prefix=None, time_format=LOGGING_TIME_FORMAT, time_zone = TIMEZONE):
+        """If max_bytes is specified on initialization, read up to that many
+        bytes when read() is called. If eol is not specified, read() will
+        read up to the first newline it receives. In both cases, if
+        timeout is specified, it will return after timeout with as many
+        bytes as it has succeeded in reading.
+
+        By default, the SerialReader will read until it encounters a newline character.
+        This behavior may be overwritten by specifying
+
+        max_bytes - if specified, and write_timeout is None, read this many bytes per record.
+                If write_timeout is not None, it may return fewer bytes.
+
+        eol - if specified, read up until encountering the specified eol
+
+        By default, the SerialReader will assume that records are encoded in UTF-8, and will
+        ignore non unicode characters it encounters. These defaults may be changed by specifying
+
+        encoding - 'utf-8' by default. If empty or None, do not attempt any decoding
+                and return raw bytes. Other possible encodings are listed in online
+                documentation here:
+                https://docs.python.org/3/library/codecs.html#standard-encodings
+
+        encoding_errors - 'ignore' by default. Other error strategies are 'strict',
+                'replace', and 'backslashreplace', described here:
+                https://docs.python.org/3/howto/unicode.html#encodings
+
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ FOR WHOI ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        prefix - Add a prefix to all incoming messages, even if blank
+
+        time_format - Format of time string added after prefix, Added ONLY if
+                      there is data.
+
+        time_zone - Time zone used when adding the time string 
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ FOR WHOI ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        command line example:
+        ```
+          # Read serial port ttyr05 expecting a LF as end of record
+          logger/listener/listen.py  --serial port=/dev/ttyr05,eol='\r'
+        ```
+        config example:
+        ```
+          class: SerialReader
+          kwargs:
+            baudrate: 4800
+            port: /dev/ttyr05
+            eol: \r
+        ```
+        """
+        super().__init__(port=port, baudrate=baudrate,
+                         bytesize=bytesize, parity=parity,
+                         stopbits=stopbits, timeout=timeout,
+                         xonxoff=xonxoff, rtscts=rtscts,
+                         write_timeout=write_timeout, dsrdtr=dsrdtr,
+                         inter_byte_timeout=inter_byte_timeout,
+                         exclusive=exclusive, encoding=encoding,
+                         encoding_errors=encoding_errors,
+                         max_bytes=max_bytes, eol=eol)
+
+        self.prefix = (
+            self._encode_str(prefix, unescape=True)
+            if prefix is not None and self.encoding
+            else None
+        )
+        self.time_format = time_format
+        self.time_zone = time_zone
+
+
+    ############################
+    def read(self):
+
+        try:
+            if self.eol:
+                record = self.serial.read_until(expected=self.eol, size=self.max_bytes)
+                # read_until()'s record includes a trailing 'eol', strip it off
+                #
+                # NOTE: But don't use rstrip which just looks explicitly for
+                #       whitespace
+                #
+                record = record.rsplit(self.eol)[0]
+            elif self.max_bytes:
+                # no stripping on this one, just use exactly what we got
+                record = self.serial.read(size=self.max_bytes)
+            else:
+                # readline()'s record includes the trailing '\n', strip it off
+                record = self.serial.readline().rstrip()
+
+            # if not record:
+            #     return None
+
+            if self.prefix:
+
+                ts = timestamp.time_str(time_format=self.time_format,
+                                        time_zone=self.time_zone)
+                ts = self._encode_str(ts, unescape=True)
+
+                record = self.prefix + b' ' + ts + b' ' + record
+            else:
+                record = record
+
+            return self._decode_bytes(record)
+
+        except KeyboardInterrupt as e:
+            raise e
+        except serial.serialutil.SerialException as e:
+            logging.error(str(e))
+            return None
---
+++ contrib/whoi/test/logger/readers/test_serial_reader.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python3
+
+import logging
+import sys
+import tempfile
+import threading
+import unittest
+import warnings
+from datetime import datetime
+
+sys.path.append('.')
+from logger.utils.simulate_data import SimSerial  # noqa: E402
+from logger.transforms.slice_transform import SliceTransform  # noqa: E402
+# from logger.readers.serial_reader import SerialReader  # noqa: E402
+from contrib.whoi.logger.readers.serial_reader import SerialReader, LOGGING_TIME_FORMAT  # noqa: E402
+
+SAMPLE_DATA = """2017-11-04T05:12:19.275337Z $HEHDT,234.76,T*1b
+2017-11-04T05:12:19.527360Z $HEHDT,234.73,T*1e
+2017-11-04T05:12:19.781738Z $HEHDT,234.72,T*1f
+2017-11-04T05:12:20.035450Z $HEHDT,234.72,T*1f
+2017-11-04T05:12:20.286551Z $HEHDT,234.73,T*1e
+2017-11-04T05:12:20.541843Z $HEHDT,234.76,T*1b
+2017-11-04T05:12:20.796684Z $HEHDT,234.81,T*13
+2017-11-04T05:12:21.047098Z $HEHDT,234.92,T*11
+2017-11-04T05:12:21.302371Z $HEHDT,235.06,T*1d
+2017-11-04T05:12:21.557630Z $HEHDT,235.22,T*1b
+2017-11-04T05:12:21.809445Z $HEHDT,235.38,T*10
+2017-11-04T05:12:22.062809Z $HEHDT,235.53,T*1d
+2017-11-04T05:12:22.312971Z $HEHDT,235.66,T*1b"""
+
+SAMPLE_MAX_BYTES_2 = ['$H',
+                      'EH',
+                      'DT',
+                      ',2',
+                      '34',
+                      '.7',
+                      '6,',
+                      'T*',
+                      '1b']
+
+SAMPLE_TIMEOUT = ['$HEHDT,234.76,T*1b',
+                  None,
+                  None,
+                  '$HEHDT,234.73,T*1e',
+                  None,
+                  None,
+                  '$HEHDT,234.72,T*1f',
+                  None,
+                  None,
+                  '$HEHDT,234.72,T*1f',
+                  None,
+                  None,
+                  '$HEHDT,234.73,T*1e']
+
+PREFIX = 'SSW'
+
+
+################################################################################
+class TestSerialReader(unittest.TestCase):
+
+
+    ############################
+    # utility to match WHOI-format timestamps
+    @staticmethod
+    def is_valid_timestamp(date_str, time_str, fmt=LOGGING_TIME_FORMAT):
+        try:
+            datetime.strptime(f'{date_str} {time_str}', fmt)
+            return True
+        except ValueError:
+            return False
+
+    ############################
+    # Set up config file and sample logfile to feed simulated serial port
+    def setUp(self):
+        warnings.simplefilter("ignore", ResourceWarning)
+
+        # Set up config file and logfile simulated serial port will read from
+        self.tmpdir = tempfile.TemporaryDirectory()
+        self.tmpdirname = self.tmpdir.name
+        logging.info('created temporary directory "%s"', self.tmpdirname)
+
+        self.port = self.tmpdirname + '/tty_gyr1'
+        self.logfile_filename = self.tmpdirname + '/NBP1700_gyr1-2017-11-04'
+        with open(self.logfile_filename, 'w') as f:
+            f.write(SAMPLE_DATA)
+
+    ############################
+    # When max_bytes is not specified, SerialReader should read port up
+    # to a newline character.
+    def test_readline(self):
+        port = self.port + '_readline'
+        sim = SimSerial(port=port, filebase=self.logfile_filename)
+        sim_thread = threading.Thread(target=sim.run)
+        sim_thread.start()
+
+        slice = SliceTransform('1:')  # we'll want to strip out timestamp
+
+        # Then read from serial port
+        s = SerialReader(port, prefix=PREFIX)
+        for line in SAMPLE_DATA.split('\n'):
+            data = slice.transform(line)
+            record = s.read()
+            logging.debug('data: %s, read: %s', data, record)
+
+            prefix, date_str, time_str, remainder = record.split(' ', 3)
+            self.assertEqual(prefix, PREFIX)
+            self.assertTrue(self.is_valid_timestamp(date_str, time_str))
+            self.assertEqual(data, remainder)
+
+    ############################
+    # When max_bytes specified, read up to that many bytes each time.
+    def test_read_bytes(self):
+        port = self.port + '_read_bytes'
+        sim = SimSerial(port=port, filebase=self.logfile_filename)
+        sim_thread = threading.Thread(target=sim.run)
+        sim_thread.start()
+
+        # Then read from serial port
+        s = SerialReader(port=port, max_bytes=2, prefix=PREFIX)
+        for data in SAMPLE_MAX_BYTES_2:
+            record = s.read()
+            logging.debug('data: %s, read: %s', data, record)
+
+            prefix, date_str, time_str, remainder = record.split(' ', 3)
+            self.assertEqual(prefix, PREFIX)
+            self.assertTrue(self.is_valid_timestamp(date_str, time_str))
+            self.assertEqual(data, remainder)
+
+    ############################
+    # For unicode craziness
+    def test_unicode(self):
+        port = self.port + '_unicode'
+        sim = SimSerial(port=port, filebase=self.logfile_filename)
+        sim_thread = threading.Thread(target=sim.run)
+        sim_thread.start()
+
+        # For some reason, the test complains unless we actually read
+        # from the port. This first SerialReader is here just to get
+        # rid of the error message that pops up from SimSerial if we
+        # don't use it.
+        s = SerialReader(port=port, prefix=PREFIX)
+        s.read()
+
+        # Now we're going to create SerialReaders with stubbed
+        # self.serial.readline methods so that when they're called,
+        # we instead get the same bit of bad unicode over and over
+        # again. We want to test that it performs correctly under
+        # conditions.
+
+        # A dummy serial readline that will feed us bad unicode
+        def dummy_readline():
+            return b'\xe2\x99\xa5\x99\xe2\x99\xa5\x00\xe2\x99\xa5\xe2\x99\xa5'
+
+        # Create a SerialReader, then replace its serial reader with a stub so
+        # we can feed it bad records.
+
+        s = SerialReader(port=port, prefix=PREFIX)
+        s.serial.readline = dummy_readline
+
+        prefix, date_str, time_str, remainder = s.read().split(' ', 3)
+        self.assertEqual(prefix, PREFIX)
+        self.assertTrue(self.is_valid_timestamp(date_str, time_str))
+        self.assertEqual('♥♥\x00♥♥', remainder)
+
+        s = SerialReader(port=port, encoding_errors='replace', prefix=PREFIX)
+        s.serial.readline = dummy_readline
+
+        prefix, date_str, time_str, remainder = s.read().split(' ', 3)
+        self.assertEqual(prefix, PREFIX)
+        self.assertTrue(self.is_valid_timestamp(date_str, time_str))
+        self.assertEqual('♥�♥\x00♥♥', remainder)
+
+    ############################
+    # When timeout specified...
+    def test_timeout(self):
+        port = self.port + '_timeout'
+        sim = SimSerial(port=port, filebase=self.logfile_filename)
+        sim_thread = threading.Thread(target=sim.run)
+        sim_thread.start()
+
+        # Then read from serial port
+        s = SerialReader(port=port, timeout=0.1, prefix=PREFIX)
+        for line in SAMPLE_TIMEOUT:
+            record = s.read()
+            logging.debug('data: %s, read: %s', line, record)
+
+            prefix, date_str, time_str, remainder = record.split(' ', 3)
+
+            self.assertEqual(prefix, PREFIX)
+            self.assertTrue(self.is_valid_timestamp(date_str, time_str))
+
+            if remainder == '':
+                remainder = None
+
+            self.assertEqual(line, remainder, msg='Note: this is a time-sensitive '
+                             'test that can fail non-deterministically. If the '
+                             'test fails, try running from the command line, e.g. '
+                             'as "logger/readers/test_serial_reader.py"')
+
+
+################################################################################
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbosity', dest='verbosity',
+                        default=0, action='count',
+                        help='Increase output verbosity')
+    args = parser.parse_args()
+
+    LOGGING_FORMAT = '%(asctime)-15s %(filename)s:%(lineno)d %(message)s'
+    logging.basicConfig(format=LOGGING_FORMAT)
+
+    LOG_LEVELS = {0: logging.WARNING, 1: logging.INFO, 2: logging.DEBUG}
+    args.verbosity = min(args.verbosity, max(LOG_LEVELS))
+    logging.getLogger().setLevel(LOG_LEVELS[args.verbosity])
+
+    # unittest.main(warnings='ignore')
+    unittest.main()
