1f97553f05d493a2b2796ada8c2925d5dea427f9
Webb Pinner <webbpinner@gmail.com>
2025-08-18 00:16:53 -0400

    Issue 459 (#460)
    * code changes to implement issue #459
    
    Adds pattern-based suffix/header to log files
    
    Allows to specify suffix, header, and header_file as dictionaries,
    enabling pattern-based selection based on the log record content.
    This allows to route records to different files with different headers/suffixes
    based on regex matching. Also fixes an issue where suffix was not optional.
    
    * streamlined code
    
    * updated docstring
    
    * Improves LogfileWriter error handling and mapping
    
    Adds enhanced error and warning logging for scenarios where suffix, header, or header file mappings fail in LogfileWriter.
    This includes checks for missing suffixes and more informative warnings for header/header file lookup failures, improving debugging and system stability.
    Also includes added tests to validate the new error handling.

--- logger/writers/logfile_writer.py
+++ logger/writers/logfile_writer.py
@@ -22,7 +22,7 @@ class LogfileWriter(Writer):
     def __init__(self, filebase=None, flush=True,
                  time_format=timestamp.TIME_FORMAT,
                  date_format=timestamp.DATE_FORMAT,
-                 split_char=' ', suffix='', header=None,
+                 split_char=' ', suffix=None, header=None,
                  header_file=None, rollover_hourly=False,
                  quiet=False):
         """Write timestamped records to a filebase. The filebase will
@@ -56,11 +56,18 @@ class LogfileWriter(Writer):
 
         split_char      Delimiter between timestamp and rest of message
 
-        suffix          string to apply to the end of the log filename
+        suffix          A suffix string to add to the log filename or a dict
+                        mapping <string>:<suffix> to select the suffix to
+                        add to a filename based on the record contents.
 
-        header          Add the specified header string to each file.
+        header          A string to add to the beginning of a new log file
+                        or a dict mapping <string>:<header> to select the
+                        header string based on the record contents.
 
-        header_file     Add the content of the specified file to each file.
+        header_file     A string containing the path to file containing a
+                        header string to add to the beginning of a new log
+                        file or a dict mapping <string>:<header_file> to select
+                        the header filepath based on the record contents.
 
         rollover_hourly Set files to truncate by hour.  By default files will
                         truncate by day
@@ -76,7 +83,8 @@ class LogfileWriter(Writer):
         self.time_format = time_format
         self.date_format = date_format
         self.split_char = split_char
-        self.suffix = suffix
+        self.suffix = suffix or ''
+
         self.header = header
         self.header_file = header_file
         self.rollover_hourly = rollover_hourly
@@ -90,8 +98,106 @@ class LogfileWriter(Writer):
             self.compiled_filebase_map = {
                 pattern: re.compile(pattern) for pattern in self.filebase
             }
+
+        # If our suffix is a dict, we're going to be doing our
+        # fancy pattern->suffix mapping.
+        self.do_suffix_mapping = isinstance(self.suffix, dict)
+
+        if self.do_suffix_mapping:
+            # Do our matches faster by precompiling
+            self.compiled_suffix_map = {
+                pattern: re.compile(pattern) for pattern in self.suffix
+            }
+
+        # If our header is a dict, we're going to be doing our
+        # fancy pattern->header mapping.
+        self.do_header_mapping = isinstance(self.header, dict)
+
+        if self.do_header_mapping:
+            # Do our matches faster by precompiling
+            self.compiled_header_map = {
+                pattern: re.compile(pattern) for pattern in self.header
+            }
+
+        # If our header_file is a dict, we're going to be doing our
+        # fancy pattern->header_file mapping.
+        self.do_header_file_mapping = isinstance(self.header_file, dict)
+
+        if self.do_header_file_mapping:
+            # Do our matches faster by precompiling
+            self.compiled_header_file_map = {
+                pattern: re.compile(pattern) for pattern in self.header_file
+            }
+
         self.current_filename = {}
         self.writer = {}
+
+    ############################
+    def fetch_suffix(self, record: str, filename_pattern: str='fixed'):
+
+        if not self.do_suffix_mapping:
+            return self.suffix
+
+        if filename_pattern != "fixed":
+            return_suffix = self.suffix.get(filename_pattern)
+
+            if return_suffix:
+                return return_suffix
+
+            if not self.quiet:
+                logging.warning('LogfileWriter.fetch_suffix() - no suffix match: "%s"!', record)
+            return
+
+        for pattern, regex in self.compiled_suffix_map.items():
+            if regex and regex.search(record):
+                return self.suffix.get(pattern)
+
+        logging.warning('LogfileWriter.fetch_suffix() - no suffix match: "%s"!', record)
+
+    ############################
+    def fetch_header(self, record: str, filename_pattern: str='fixed'):
+
+        if not self.do_header_mapping:
+            return self.header
+
+        if filename_pattern != "fixed":
+            return_header = self.header.get(filename_pattern)
+
+            if return_header:
+                return return_header
+
+            if not self.quiet:
+                logging.warning('LogfileWriter.fetch_header() - no header match: "%s"', record)
+            return ''
+
+        for pattern, regex in self.compiled_header_map.items():
+            if regex and regex.search(record):
+                return self.header.get(pattern, '')
+
+        logging.warning('LogfileWriter.fetch_header() - no header match: "%s"', record)
+        return ''
+
+    ############################
+    def fetch_header_file(self, record: str, filename_pattern: str='fixed'):
+
+        if not self.do_header_file_mapping:
+            return self.header_file
+
+        if filename_pattern != "fixed":
+            return_header_file = self.header_file.get(filename_pattern)
+
+            if return_header_file:
+                return return_header_file
+
+            if not self.quiet:
+                logging.warning('LogfileWriter.fetch_header_file() - no header file match: "%s"', record)
+            return
+
+        for pattern, regex in self.compiled_header_file_map.items():
+            if regex and regex.search(record):
+                return self.header_file.get(pattern)
+
+        logging.warning('LogfileWriter.fetch_header_file() - no header file match: "%s"', record)
 
     ############################
     def write(self, record: Union[str, DASRecord, dict]):
@@ -134,8 +240,7 @@ class LogfileWriter(Writer):
         hr_str = self.rollover_hourly and \
             timestamp.date_str(ts, date_format='_%H00') or ""
         date_str = timestamp.date_str(ts, date_format=self.date_format)
-        time_str = date_str + hr_str + self.suffix
-        logging.debug('LogfileWriter time_str: %s', time_str)
+        time_str = date_str + hr_str
 
         # Figure out where we're going to write
         if self.do_filebase_mapping:
@@ -147,7 +252,13 @@ class LogfileWriter(Writer):
                                     f'options for record "{record}"')
         else:
             pattern = 'fixed'  # just an arbitrary fixed pattern
-            filename = self.filebase + '-' + time_str
+
+            suffix = self.fetch_suffix(record, pattern)
+            if suffix is None:
+                logging.error(f'System error: found no suffix matching record: "{record}"!')
+                return None
+
+            filename = self.filebase + '-' + time_str + suffix
             self.write_filename(record, pattern, filename)
 
     ############################
@@ -156,7 +267,7 @@ class LogfileWriter(Writer):
         # Find the compiled regex matching the pattern
         regex = self.compiled_filebase_map.get(pattern)
         if not regex:
-            logging.error(f'System error: found no regex pattern matching "{pattern}"!')
+            logging.error(f'System error: found no regex matching pattern: "{pattern}"!')
             return None
 
         # If the pattern isn't in this record, go home quietly
@@ -169,7 +280,12 @@ class LogfileWriter(Writer):
             logging.error(f'System error: found no filebase matching pattern "{pattern}"!')
             return None
 
-        filename = filebase + '-' + time_str
+        suffix = self.fetch_suffix(record, pattern)
+        if suffix is None:
+            logging.error(f'System error: found no suffix matching pattern: "{pattern}"!')
+            return None
+
+        filename = filebase + '-' + time_str + suffix
         self.write_filename(record, pattern, filename)
         return True
 
@@ -181,11 +297,15 @@ class LogfileWriter(Writer):
 
         # Are we currently writing to this file? If not, open/create it.
         if not filename == self.current_filename.get(pattern):
-            logging.info('LogfileWriter opening new file: %s', filename)
+
+            # calculate header/header_file and suffix
+            header = self.fetch_header(record, pattern) if self.do_header_mapping else self.header
+            header_file = self.fetch_header_file(record, pattern) if self.do_header_file_mapping else self.header_file
+
             self.current_filename[pattern] = filename
             self.writer[pattern] = FileWriter(filename=filename,
-                                              header=self.header,
-                                              header_file=self.header_file,
+                                              header=header,
+                                              header_file=header_file,
                                               flush=self.flush)
         # Now, if our logic is correct, should *always* have a matching_writer
         matching_writer = self.writer.get(pattern)
