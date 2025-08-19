f387b0b043e1b8f940badbae414c6b6038b9caea
Webb Pinner <webbpinner@gmail.com>
2025-08-19 14:51:02 -0400

    Adds Sealog reader
    Adds a reader that connects to a Sealog websocket server.
    
    This reader retrieves events from the server and converts them into DASRecord objects for further processing.  It handles reconnection logic and different SSL configurations for secure connections. A sample configuration file is included for testing.

---
+++ logger/readers/sealog_reader.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python3
+
+import asyncio
+import logging
+import queue
+import ssl
+import json
+import threading
+import time
+import sys
+from os.path import dirname, realpath
+import websockets
+
+sys.path.append(dirname(dirname(dirname(realpath(__file__)))))
+from logger.utils.das_record import DASRecord  # noqa: E402
+from logger.utils.timestamp import timestamp  # noqa: E402
+
+################################################################################
+class SealogReader():
+    """Connect to a websocket served by a WebsocketWriter, and service read()
+    requests from it.
+    """
+
+    def __init__(self, uri, client_wsid=None, subs=None, check_cert=False):
+        """
+        ```
+        uri - Hostname, port and protocol, (e.g. wss://localhost:8080) of the
+              target Sealog Server
+
+        client_wsid - Unique identifier, useful when running multiple
+                      SealogReaders
+
+        subs - Websocket Pub/Subs to subscribe to when listening to the Sealog
+               Server
+
+        check_cert - If True, and uri protocol is 'wss', check the server's TLS
+                     certificate for validity; if a str, use as local filepath
+                     location of .pem file to check against.
+        ```
+        """
+        self.uri = uri
+        self.client_wsid = client_wsid or 'OpenRVDAS_Sealog_Listener'
+        self.subs = subs or ['/ws/status/newEvents', '/ws/status/updateEvents']
+        self.check_cert = check_cert
+
+        self.ping = {
+            'type': 'ping',
+            'id': self.client_wsid
+        }
+
+        self.hello = {
+            'type': 'hello',
+            'id': self.client_wsid,
+            'version': '2',
+            'subs': self.subs
+        }
+
+        # We won't initialize our websocket connection until the first read()
+        # call. At that point we'll launch an async process in a separate
+        # thread that will wait for data from the websocket and put it in a
+        # queue that read() will pop from.
+        self.websocket_thread = None
+        self.queue = queue.Queue()
+        self.quit_flag = False
+
+
+    ############################
+    def _start_websocket(self):
+        """We'll run this in a separate thread as soon as we get our first
+        call to read()."""
+
+        ############################
+        def _event_to_das_record(event):
+
+            fields = {
+                'event_id': event.get('id'),
+                'event_value': event.get('event_value'),
+                'event_author': event.get('event_author'),
+                'event_free_text': event.get('event_free_text'),
+                **{
+                    f"event_option_{option['event_option_name']}": option["event_option_value"]
+                    for option in event.get("event_options", [])
+                }
+            }
+
+            return DASRecord(
+                data_id=self.client_wsid,
+                message_type='sealog_event',
+                timestamp=timestamp(event.get('ts')),
+                fields=fields
+            )
+
+        ############################
+        async def _websocket_loop(self):
+            """Asynchronous inner function that will connect to websocket,
+            iteratively try to read from it and put the result in our queue.
+            """
+            # Iterate if we lose the websocket for some reason other than a 'quit'
+            while not self.quit_flag:
+                try:
+                    if self.uri.find('wss://') == 0:  # using wss
+                        # If check_cert is a str, take it as the location of the
+                        # .pem file we'll check for validity. Otherwise, if not
+                        # False, take as a bool to verify by own means.
+                        ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+                        if self.check_cert:
+                            if isinstance(self.check_cert, str):
+                                ssl_context.load_verify_locations(self.check_cert)
+                            else:
+                                ssl_context.verify_mode = ssl.CERT_REQUIRED
+                        else:
+                            ssl_context.check_hostname = False
+                            ssl_context.verify_mode = ssl.CERT_NONE
+
+                    else:  # not using wss
+                        ssl_context = None
+
+                    logging.debug(f'SealogReader connecting to {self.uri}')
+                    async with websockets.connect(self.uri, ssl=ssl_context) as ws:  # type: ignore
+
+                        await ws.send(json.dumps(self.hello))
+                        logging.info(f'Connected to SealogReader at {self.uri}')
+
+                        while not self.quit_flag:
+                            msg = await ws.recv()
+                            msg_obj = json.loads(msg)
+
+                            if msg_obj['type'] and msg_obj['type'] == 'ping':
+                                await ws.send(json.dumps(self.ping))
+                                continue
+
+                            if msg_obj['type'] and msg_obj['type'] == 'pub':
+
+                                event = msg_obj['message']
+                                logging.debug(f'SealogReader got record {json.dumps(event, indent=2)}')
+                                self.queue.put(_event_to_das_record(event))
+
+                except BrokenPipeError:
+                    logging.info(f'SealogReader BrokenPipeError connecting to {self.uri}')
+                except AttributeError as e:
+                    logging.warning(f'SealogReader websocket loop error: {e}')
+                except websockets.exceptions.ConnectionClosed:  # type: ignore
+                    logging.info('SealogReader lost websocket connection to '
+                                 'data server; trying to reconnect.')
+                    await asyncio.sleep(0.2)
+                except OSError as e:
+                    logging.info('Unable to connect to websocket. Sleeping to try again...')
+                    logging.info(f'Connection error: {e}')
+                    await asyncio.sleep(2)
+
+        # In the outer function, get a new event loop and fire up the
+        # inner, async routine.
+        # Could we also use asyncio.ensure_future(_websocket_loop(self)) ?
+        websocket_event_loop = asyncio.new_event_loop()
+        websocket_event_loop.run_until_complete(_websocket_loop(self))
+        websocket_event_loop.close()
+
+    ############################
+    def quit(self, seconds=0):
+        """Sleep N seconds, then signal quit."""
+        time.sleep(seconds)
+        self.quit_flag = True
+
+    ############################
+    def read(self):
+        """Read/wait for data from the websocket."""
+
+        # If we've not yet fired up the websocket thread, do that now.
+        if not self.websocket_thread:
+            self.websocket_thread = threading.Thread(
+                name='websocket_thread',
+                target=self._start_websocket,
+                daemon=True)
+            self.websocket_thread.start()
+
+        # Use a timeout in our queue get() so we can periodically check if
+        # we've gotten a 'quit'
+        while not self.quit_flag:
+            try:
+                result = self.queue.get(block=True, timeout=2)
+                logging.debug('Got result from queue: %s', result)
+                return result
+            except queue.Empty:
+                logging.debug('get() timed out - trying again')
+
+        # If we've fallen out because of a quit...
+        return None
